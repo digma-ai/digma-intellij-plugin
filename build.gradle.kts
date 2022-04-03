4303797208b7babf891bde3a58d924e4d7f1780f
shalom <shalom938@gmail.com>
2022-04-03 07:04:41 +0300

    register editor listener early

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,6 @@ intellij {
     version.set(properties("platformVersion"))
     type.set(properties("platformType"))
     plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
-//    downloadSources.set(false)
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin

57a3b9dde9c1a1126ee95077b6e4990196bbea9e
shalom <shalom938@gmail.com>
2022-04-05 16:13:35 +0300

    kotlin compile issue

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     `groovy-gradle-plugin`
     `java-gradle-plugin`
@@ -13,12 +15,13 @@ dependencies {
     implementation("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.5.2")
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
-    }
-}
 
+
+java {
+    //some issue with KotlinCompile needs that, usually that's not necessary
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
 
 tasks.test {
     testLogging {
@@ -48,6 +51,14 @@ tasks.test {
 
 
 tasks{
+
+    withType<JavaCompile> {
+        options.release.set(11)
+    }
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
     jar{
         //don't need the jar unless there is at least one gradlePlugin
         enabled = false

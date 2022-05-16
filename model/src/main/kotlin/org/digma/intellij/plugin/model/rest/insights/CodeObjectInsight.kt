59bd845e4e53e63ce0e3d5b89bf635946667d5c8
shalom <shalom938@gmail.com>
2022-05-16 10:08:31 +0300

    insights panel take 1

--- model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/CodeObjectInsight.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/CodeObjectInsight.kt
@@ -1,11 +1,9 @@
 package org.digma.intellij.plugin.model.rest.insights
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import org.digma.intellij.plugin.model.InsightType
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes(
     JsonSubTypes.Type(value = HotspotInsight::class, name = "HotSpot"),

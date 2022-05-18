9f8f8b1e3cc9892717dd065d2f8757281cc5e439
arik-dig <asher@digma.ai>
2022-05-18 14:12:36 +0300

    model object is SortedSet
    Signed-off-by: arik-dig <asher@digma.ai>

--- model/src/main/kotlin/org/digma/intellij/plugin/ui/model/listview/GroupListViewItem.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/ui/model/listview/GroupListViewItem.kt
@@ -2,11 +2,13 @@ package org.digma.intellij.plugin.ui.model.listview
 
 import java.util.*
 
-abstract class GroupListViewItem(sortIndex: Int, groupId: String) : ListViewItem<Any>(Object(), sortIndex, groupId) {
-
-    private val items: SortedSet<ListViewItem<Any>> = TreeSet(Comparator.comparingInt(ListViewItem<Any>::sortIndex))
+abstract class GroupListViewItem(sortIndex: Int, groupId: String) : ListViewItem<SortedSet<ListViewItem<Any>>>(
+    TreeSet(Comparator.comparingInt(ListViewItem<Any>::sortIndex)),
+    sortIndex,
+    groupId
+) {
 
     fun addItem(item: ListViewItem<Any>) {
-        items.add(item)
+        modelObject.add(item)
     }
 }

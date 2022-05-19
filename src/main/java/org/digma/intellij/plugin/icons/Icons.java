de69c93843fe1803e62d47cf818ad4e1e631b8a0
arik-dig <asher@digma.ai>
2022-05-19 15:24:26 +0300

    bring some icons
    Signed-off-by: arik-dig <asher@digma.ai>

--- src/main/java/org/digma/intellij/plugin/icons/Icons.java
+++ src/main/java/org/digma/intellij/plugin/icons/Icons.java
@@ -7,10 +7,22 @@ import javax.swing.*;
 @SuppressWarnings("unused")
 public final class Icons {
 
-    private Icons() {}
+    private Icons() {
+    }
 
     public static final Icon TOOL_WINDOW = IconLoader.getIcon("/icons/toolwindow.png", Icons.class);
     public static final Icon INSIGHT_METHOD_SCOPE = IconLoader.getIcon("/icons/method.png", Icons.class);
 
     public static final Icon HOTSPOT = IconLoader.getIcon("/icons/hotspot.png", Icons.class);
+
+    static public class Insight {
+
+        public static final Icon BOTTLENECK = IconLoader.getIcon("/icons/bottleneck.png", Icons.class);
+        public static final Icon LOW_USAGE = IconLoader.getIcon("/icons/gauge_low.png", Icons.class);
+        public static final Icon NORMAL_USAGE = IconLoader.getIcon("/icons/gauge_normal.png", Icons.class);
+        public static final Icon HIGH_USAGE = IconLoader.getIcon("/icons/gauge_high.png", Icons.class);
+        public static final Icon SLOW = IconLoader.getIcon("/icons/slow.png", Icons.class);
+        public static final Icon TARGET = IconLoader.getIcon("/icons/target.png", Icons.class);
+        public static final Icon THIN_TARGET = IconLoader.getIcon("/icons/thin-target.png", Icons.class);
+    }
 }

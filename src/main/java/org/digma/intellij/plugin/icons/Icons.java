8b7d4d41cddfdf21606863e83b05bcd798613d7a
arik-dig <asher@digma.ai>
2022-05-22 15:42:43 +0300

    add smaller icons
    Signed-off-by: arik-dig <asher@digma.ai>

--- src/main/java/org/digma/intellij/plugin/icons/Icons.java
+++ src/main/java/org/digma/intellij/plugin/icons/Icons.java
@@ -20,9 +20,9 @@ public final class Icons {
     static public class Insight {
 
         public static final Icon BOTTLENECK = IconLoader.getIcon("/icons/insight/bottleneck.png", Icons.class);
-        public static final Icon LOW_USAGE = IconLoader.getIcon("/icons/insight/gauge_low.png", Icons.class);
-        public static final Icon NORMAL_USAGE = IconLoader.getIcon("/icons/insight/gauge_normal.png", Icons.class);
-        public static final Icon HIGH_USAGE = IconLoader.getIcon("/icons/insight/gauge_high.png", Icons.class);
+        public static final Icon LOW_USAGE = IconLoader.getIcon("/icons/insight/gauge_low-32.png", Icons.class);
+        public static final Icon NORMAL_USAGE = IconLoader.getIcon("/icons/insight/gauge_normal-32.png", Icons.class);
+        public static final Icon HIGH_USAGE = IconLoader.getIcon("/icons/insight/gauge_high-32.png", Icons.class);
         public static final Icon SLOW = IconLoader.getIcon("/icons/insight/slow.png", Icons.class);
         public static final Icon TARGET = IconLoader.getIcon("/icons/insight/target.png", Icons.class);
         public static final Icon THIN_TARGET = IconLoader.getIcon("/icons/insight/thin-target.png", Icons.class);

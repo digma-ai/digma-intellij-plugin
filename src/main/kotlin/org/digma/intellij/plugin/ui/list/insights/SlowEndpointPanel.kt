2ba9bd14be4762d821529fab72f04ffed00f2ecc
arik-dig <asher@digma.ai>
2022-05-23 17:41:34 +0300

    Slow Endpoint similar to TrafficUsage
    Signed-off-by: arik-dig <asher@digma.ai>

--- src/main/kotlin/org/digma/intellij/plugin/ui/list/insights/SlowEndpointPanel.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/list/insights/SlowEndpointPanel.kt
@@ -1,39 +1,46 @@
 package org.digma.intellij.plugin.ui.list.insights
 
-import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.components.JBPanel
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.util.ui.JBUI
 import org.digma.intellij.plugin.icons.Icons
 import org.digma.intellij.plugin.model.rest.insights.Duration
 import org.digma.intellij.plugin.model.rest.insights.SlowEndpointInsight
+import org.digma.intellij.plugin.ui.common.asHtml
+import org.digma.intellij.plugin.ui.common.iconPanelGrid
+import java.awt.BorderLayout
 import java.math.BigDecimal
 import java.math.RoundingMode
+import javax.swing.Box
+import javax.swing.BoxLayout
+import javax.swing.JLabel
+import javax.swing.SwingConstants
 
 
-fun slowEndpointPanel(insight: SlowEndpointInsight): DialogPanel {
-    val result = panel {
-        twoColumnsRow(
-            column1 = {
-                panel {
-                    row {
-                        label("Slow Endpoint").bold()
-                    }
-                    row {
-                        text(genContentAsHtml(insight))
-                    }
-                }
-            },
-            column2 = {
-                panel {
-                    row {
-                        icon(Icons.Insight.SLOW)
-                    }
-                    row {
-                        label(evalDuration(insight.median))
-                    }
-                }
-            }
-        )
+fun slowEndpointPanel(insight: SlowEndpointInsight): JBPanel<JBPanel<*>> {
+    val title = panel {
+        row {
+            label("Slow Endpoint").bold()
+        }
     }
+
+    val descriptionNearIcon = evalDuration(insight.median)
+    val iconPanel = iconPanelGrid(Icons.Insight.SLOW, descriptionNearIcon)
+    iconPanel.border = JBUI.Borders.empty(10)
+
+    val contentPanel = JBPanel<JBPanel<*>>()
+    contentPanel.layout = BorderLayout()
+
+    val message = JLabel(genContentAsHtml(insight), SwingConstants.LEFT)
+
+    contentPanel.add(title, BorderLayout.NORTH)
+    contentPanel.add(message, BorderLayout.CENTER)
+
+    val result = JBPanel<JBPanel<*>>()
+    result.layout = BoxLayout(result, BoxLayout.X_AXIS)
+    result.add(contentPanel)
+    result.add(Box.createHorizontalStrut(20))
+    result.add(iconPanel)
     result.toolTipText = genToolTip(insight)
 
     return result
@@ -51,11 +58,8 @@ fun evalDuration(duration: Duration): String {
 }
 
 fun genContentAsHtml(insight: SlowEndpointInsight): String {
-    return "<p>On average requests are slower than </p><p>other endpoints by <span style=\"color:red\">${
-        computePercentageDiff(
-            insight
-        )
-    }</span></p>"
+    val pctVal = computePercentageDiff(insight)
+    return asHtml("On average requests are slower than other endpoints by <span style=\"color:red\">${pctVal}")
 }
 
 fun computePercentageDiff(insight: SlowEndpointInsight): String {

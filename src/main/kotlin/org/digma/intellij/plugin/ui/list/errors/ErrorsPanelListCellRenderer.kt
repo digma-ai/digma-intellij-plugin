8809c804d0d32f65caff0d5e1fa1f4917d546b35
arik-dig <asher@digma.ai>
2022-05-26 19:26:48 +0300

    start putting data into panel
    Signed-off-by: arik-dig <asher@digma.ai>

--- src/main/kotlin/org/digma/intellij/plugin/ui/list/errors/ErrorsPanelListCellRenderer.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/list/errors/ErrorsPanelListCellRenderer.kt
@@ -1,30 +1,41 @@
 package org.digma.intellij.plugin.ui.list.errors
 
 import com.intellij.openapi.project.Project
+import com.intellij.ui.dsl.builder.panel
+import org.digma.intellij.plugin.model.rest.errors.CodeObjectError
 import org.digma.intellij.plugin.ui.list.AbstractPanelListCellRenderer
 import org.digma.intellij.plugin.ui.model.listview.ListViewItem
-import java.awt.BorderLayout
-import javax.swing.JLabel
 import javax.swing.JPanel
-
 
 class ErrorsPanelListCellRenderer : AbstractPanelListCellRenderer() {
 
 
     override fun createPanel(project: Project, value: ListViewItem<*>, index: Int): JPanel {
-        return getOrCreatePanel(project, index, value)
+        return getOrCreatePanel(project, index, value as ListViewItem<CodeObjectError>)
     }
 
     @Suppress("UNCHECKED_CAST")
-    private fun getOrCreatePanel(project: Project, index: Int, value: ListViewItem<*>): JPanel {
+    private fun getOrCreatePanel(project: Project, index: Int, value: ListViewItem<CodeObjectError>): JPanel {
 
-        val panel = JPanel()
-        panel.layout = BorderLayout()
-        val label = JLabel()
-        label.text = "this is an error $index"
-        panel.add(label,BorderLayout.CENTER)
+        val model = value.modelObject
 
-        return panel
+        val result = panel {
+            //temporary: need to implement logic
+            row {
+                link(model.name) {
+                    //error.codeObjectId
+                    println("In action")
+                }
+                var from = "From me"
+//                if (insight.codeObjectId != error.sourceCodeObjectId) {
+//                    from = "From ${error.sourceCodeObjectId.split("\$_\$")[0]}"
+//                }
+                label(from)
+                label("Score: ${model.scoreInfo.score}")
+            }
+        }
+
+        return result
     }
 
 }

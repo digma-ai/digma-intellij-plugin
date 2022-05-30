b6901bde08892c61ebc58c65285562527f13e2f8
shalom <shalom938@gmail.com>
2022-05-30 14:17:38 +0300

    fix null exception when there is no environment

--- ide-common/src/main/java/org/digma/intellij/plugin/analytics/AnalyticsService.java
+++ ide-common/src/main/java/org/digma/intellij/plugin/analytics/AnalyticsService.java
@@ -85,8 +85,12 @@ public class AnalyticsService implements AnalyticsProvider, Disposable {
             try {
                 return method.invoke(analyticsProvider, args);
             } catch (Exception e) {
-                String argsStr = Arrays.stream(args).map(Object::toString).collect(Collectors.joining(","));
-                Log.error(LOGGER, e, "error invoking AnalyticsProvide.{}({}), exception {}", method.getName(),argsStr,e);
+                if (args != null && args.length > 0) {
+                    String argsStr = Arrays.stream(args).map(Object::toString).collect(Collectors.joining(","));
+                    Log.error(LOGGER, e, "error invoking AnalyticsProvide.{}({}), exception {}", method.getName(), argsStr, e);
+                }else{
+                    Log.error(LOGGER, e, "error invoking AnalyticsProvide.{}, exception {}", method.getName(), e);
+                }
                 //todo: we probably need to show some error message to user
                 return null;
             }

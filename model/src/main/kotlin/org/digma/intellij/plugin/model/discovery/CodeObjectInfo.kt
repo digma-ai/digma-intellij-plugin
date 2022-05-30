ca33255efb1b618bf26b9c97687e24f40360d17e
arik-dig <asher@digma.ai>
2022-05-30 15:57:34 +0300

    functions to extract method and class names
    Signed-off-by: arik-dig <asher@digma.ai>

--- model/src/main/kotlin/org/digma/intellij/plugin/model/discovery/CodeObjectInfo.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/discovery/CodeObjectInfo.kt
@@ -3,5 +3,16 @@ package org.digma.intellij.plugin.model.discovery
 interface CodeObjectInfo {
     val id: String
 
-    fun idWithType():String
+    fun idWithType(): String
+
+    companion object {
+
+        fun extractMethodName(codeObjectId: String): String {
+            return codeObjectId.split("\$_\$")[1]
+        }
+
+        fun extractFqnClassName(codeObjectId: String): String {
+            return codeObjectId.split("\$_\$")[0]
+        }
+    }
 }

3d66276cc9dfe3491427d29ece1adb00b4d2ab2b
shalom <shalom938@gmail.com>
2022-06-06 21:05:45 +0300

    prefered focusable component

--- rider/Digma.Rider.Plugin/Digma.Rider/Highlighting/MethodInsightsProvider.cs
+++ rider/Digma.Rider.Plugin/Digma.Rider/Highlighting/MethodInsightsProvider.cs
@@ -1,13 +1,24 @@
 using System.Collections.Generic;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Daemon.CodeInsights;
+using JetBrains.ReSharper.Feature.Services.Navigation;
+using JetBrains.ReSharper.Psi;
 using JetBrains.Rider.Model;
+using JetBrains.Util;
+using static Digma.Rider.Logging.Logger;
 
 namespace Digma.Rider.Highlighting
 {
     [SolutionComponent]
     public class MethodInsightsProvider : ICodeInsightsProvider
     {
+        private readonly ILogger _logger;
+
+        public MethodInsightsProvider(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public bool IsAvailableIn(ISolution solution)
         {
             return true;
@@ -15,12 +26,20 @@ namespace Digma.Rider.Highlighting
 
         public void OnClick(CodeInsightsHighlighting highlighting, ISolution solution)
         {
-            //todo : implement
+            Log(_logger, "OnClick invoked for {0}", highlighting.DeclaredElement.ShortName);
+            using (CompilationContextCookie.GetExplicitUniversalContextIfNotSet())
+            {
+                highlighting.DeclaredElement.Navigate(true);
+            }
         }
 
         public void OnExtraActionClick(CodeInsightsHighlighting highlighting, string actionId, ISolution solution)
         {
-            //todo : implement
+            Log(_logger, "OnExtraActionClick invoked for {0}", highlighting.DeclaredElement);
+            using (CompilationContextCookie.GetExplicitUniversalContextIfNotSet())
+            {
+                highlighting.DeclaredElement.Navigate(true);
+            }
         }
 
         public string ProviderId => nameof(MethodInsightsProvider);

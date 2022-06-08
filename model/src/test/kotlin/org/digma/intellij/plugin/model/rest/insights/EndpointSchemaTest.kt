ca88f701935dd57f5c10b08858bf372dbdd1f6dd
arik-dig <asher@digma.ai>
2022-06-08 10:06:27 +0300

    EndpointSchemaTest
    Signed-off-by: arik-dig <asher@digma.ai>

---
+++ model/src/test/kotlin/org/digma/intellij/plugin/model/rest/insights/EndpointSchemaTest.kt
@@ -0,0 +1,39 @@
+package org.digma.intellij.plugin.model.rest.insights
+
+import org.digma.intellij.plugin.model.rest.insights.EndpointSchema.Companion.adjustHttpRouteIfNeeded
+import org.digma.intellij.plugin.model.rest.insights.EndpointSchema.Companion.getShortRouteName
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class EndpointSchemaTest {
+
+    @Test
+    fun getShortRouteNameShouldWorkForHttpSchema() {
+        assertEquals("get /hello", getShortRouteName("epHTTP:get /hello"))
+    }
+
+    @Test
+    fun getShortRouteNameShouldWorkForRpcSchema() {
+        assertEquals("hello.world", getShortRouteName("epRPC:hello.world"))
+    }
+
+    @Test
+    fun getShortRouteNameShouldWorkEvenWhenNonRecognizedSchema() {
+        assertEquals("whats.up.dude", getShortRouteName("whats.up.dude"))
+    }
+
+    @Test
+    fun adjustHttpRouteIfNeededShouldAdjustWhenNoSchema() {
+        val normalUsageInsight = NormalUsageInsight("123", "get /yes", 3)
+        adjustHttpRouteIfNeeded(normalUsageInsight)
+        assertEquals("epHTTP:get /yes", normalUsageInsight.route)
+    }
+
+    @Test
+    fun adjustHttpRouteIfNeededShouldSkipAdjustWhenSchemaExists() {
+        val normalUsageInsight = NormalUsageInsight("456", "epHTTP:post /letsgo", 8)
+        adjustHttpRouteIfNeeded(normalUsageInsight)
+        assertEquals("epHTTP:post /letsgo", normalUsageInsight.route)
+    }
+
+}

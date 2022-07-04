66c603d0122495a8fe57a92c1947488feef96808
arik-dig <asher@digma.ai>
2022-07-04 13:28:08 +0300

    MethodParameter - add support to generics and arrays
    Signed-off-by: arik-dig <asher@digma.ai>

--- model/src/main/kotlin/org/digma/intellij/plugin/model/discovery/MethodParameter.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/discovery/MethodParameter.kt
@@ -5,11 +5,32 @@ data class MethodParameter(
     val name: String
 ) {
 
+    companion object {
+        private val arrayRegex = Regex("\\[,*]$")
+    }
+
+    // should meet the name of parameter as used in stack trace
     fun typeShortName(): String {
-        if (typeFqn == null) {
-            return "";
+        val arrayMatchResult = arrayRegex.find(typeFqn, 0)
+
+        val arrayPart: String
+        if (arrayMatchResult != null) {
+            arrayPart = arrayMatchResult.value
+        } else {
+            arrayPart = ""
         }
-        return typeFqn.split('.').last()
+
+        val firstIndexOfSquaredParenthesisOpening = typeFqn.indexOf('[')
+        val relevantTypeFqn =
+            if (firstIndexOfSquaredParenthesisOpening < 0) {
+                typeFqn
+            } else {
+                // has SquaredParenthesisOpening - generic or array, never mind just get the first part
+                typeFqn.substring(0, firstIndexOfSquaredParenthesisOpening)
+            }
+        val shortNameBeforeArray = relevantTypeFqn.split('.').last()
+
+        return "$shortNameBeforeArray$arrayPart"
     }
 
 }
---
+++ model/src/test/kotlin/org/digma/intellij/plugin/model/discovery/MethodParameterTest.kt
@@ -0,0 +1,55 @@
+package org.digma.intellij.plugin.model.discovery
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MethodParameterTest {
+
+    @Test
+    fun typeShortNameShouldWorkOnSimpleType() {
+        assertTypeShortName("System.String", "String")
+        assertTypeShortName("System.Int32", "Int32")
+        assertTypeShortName("Hello.World", "World")
+        assertTypeShortName("Abc", "Abc")
+    }
+
+    @Test
+    fun typeShortNameShouldWorkOnArray() {
+        assertTypeShortName("System.String[]", "String[]")
+        assertTypeShortName("System.Int32[]", "Int32[]")
+        assertTypeShortName("Hello.World[]", "World[]")
+        assertTypeShortName("Abc[]", "Abc[]")
+    }
+
+    @Test
+    fun typeShortNameShouldWorkOnMultiDimensionalArrays() {
+        assertTypeShortName("System.String[,]", "String[,]")
+        assertTypeShortName("System.Int32[,]", "Int32[,]")
+        assertTypeShortName("Hello.World[,]", "World[,]")
+        assertTypeShortName("Abc[,]", "Abc[,]")
+    }
+
+    @Test
+    fun typeShortNameWithGenerics() {
+        // parameter defined as "IList<string> names"
+        assertTypeShortName("System.Collections.Generic.IList`1[T -> System.String]", "IList`1")
+    }
+
+    @Test
+    fun typeShortNameWithListOfArray() {
+        // parameter defined as "IList<string[]> listOfArray"
+        assertTypeShortName("System.Collections.Generic.IList`1[T -> System.String[]]", "IList`1")
+    }
+
+    @Test
+    fun typeShortNameWithArrayOfList() {
+        // parameter defined as "IList<int>[] arrayOfList"
+        assertTypeShortName("System.Collections.Generic.IList`1[T -> System.Int32][]", "IList`1[]")
+    }
+
+    private fun assertTypeShortName(typeFqn: String, expectedShortName: String) {
+        val mp = MethodParameter(typeFqn, "whatever")
+        assertEquals(expectedShortName, mp.typeShortName(), "typeShortName")
+    }
+
+}

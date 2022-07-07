dd9d990ec812f47dc3230f241fa8f5949ba75068
arik-dig <asher@digma.ai>
2022-07-07 11:58:17 +0300

    ParameterShortType + tests

--- rider/Digma.Rider.Plugin/Digma.Rider/Discovery/Identities.cs
+++ rider/Digma.Rider.Plugin/Digma.Rider/Discovery/Identities.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using Digma.Rider.Util;
+using System.Text.RegularExpressions;
 using JetBrains.Annotations;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
@@ -23,6 +24,50 @@ namespace Digma.Rider.Discovery
             return sourceFile.GetLocation().ToUri().ToString();
         }
 
+        static readonly Regex arrayRegex = new Regex("\\[,*]$");
+
+        static string GetArraysPart(string typeFqn)
+        {
+            var arraysValue = "";
+            var trimmableValue = typeFqn;
+
+            var arrayMatchResult = arrayRegex.Match(trimmableValue, 0);
+            while (arrayMatchResult != null && arrayMatchResult.Success)
+            {
+                arraysValue = arrayMatchResult.Value + arraysValue;
+                trimmableValue = trimmableValue.Substring(0, arrayMatchResult.Index);
+
+                arrayMatchResult = arrayRegex.Match(trimmableValue, 0);
+            }
+
+            return arraysValue;
+        }
+        
+        public static string ParameterShortType(string typeFqn)
+        {
+            string refSignToAppend = "";
+            string localTypeFqn = typeFqn;
+            if (typeFqn.EndsWith("&"))
+            {
+                refSignToAppend = "&";
+                localTypeFqn = typeFqn.TrimEnd('&');
+            }
+
+            var arraysValuePart = GetArraysPart(localTypeFqn);
+
+            var firstIndexOfSquaredParenthesisOpening = localTypeFqn.IndexOf('[');
+            var relevantTypeFqn = localTypeFqn;
+            if (firstIndexOfSquaredParenthesisOpening >= 0) 
+            {
+                // has SquaredParenthesisOpening - generic or array, never mind just get the first part
+                relevantTypeFqn = localTypeFqn.Substring(0, firstIndexOfSquaredParenthesisOpening);
+            }
+
+            var shortNameBeforeArray = relevantTypeFqn.Split('.').Last();
+
+            return $"{shortNameBeforeArray}{arraysValuePart}{refSignToAppend}";            
+        }
+        
         public static string ComputeParametersPart(ICSharpFunctionDeclaration functionDeclaration)
         {
             var parametersOwner = functionDeclaration.GetParametersOwner();

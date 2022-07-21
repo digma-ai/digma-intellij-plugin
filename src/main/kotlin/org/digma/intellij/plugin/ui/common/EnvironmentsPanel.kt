f5b0724e0db4e23775ed70288d1ff1849203ca1b
arik-dig <asher@digma.ai>
2022-07-21 10:16:26 +0300

    sort environments - remove irrelevant LOCALs
    Signed-off-by: arik-dig <asher@digma.ai>

--- src/main/kotlin/org/digma/intellij/plugin/ui/common/EnvironmentsPanel.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/common/EnvironmentsPanel.kt
@@ -5,6 +5,7 @@ import com.intellij.ui.components.ActionLink
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.dsl.builder.MutableProperty
 import com.intellij.ui.dsl.builder.panel
+import org.digma.intellij.plugin.common.CommonUtils
 import org.digma.intellij.plugin.ui.model.environment.EnvironmentsSupplier
 import java.util.function.Supplier
 
@@ -36,9 +37,11 @@ private class SingleEnvPanelMutableProperty(val envsSupplierSupplier: Supplier<E
     override fun get(): List<SingleEnvPanel> {
         val envsSupplier = envsSupplierSupplier.get()
 
+        val relevantEnvs = buildRelevantSortedEnvironments(envsSupplier)
+
         val list = ArrayList<SingleEnvPanel>()
 
-        for (currEnv in envsSupplier.getEnvironments()) {
+        for (currEnv in relevantEnvs) {
             val isSelectedEnv = currEnv.contentEquals(envsSupplier.getCurrent())
             val linkText = buildLinkText(currEnv, isSelectedEnv)
             val link = ActionLink(asHtml(linkText)) {
@@ -54,12 +57,48 @@ private class SingleEnvPanelMutableProperty(val envsSupplierSupplier: Supplier<E
         return list
     }
 
-    fun buildLinkText(currEnv: String, isSelectedEnv: Boolean): String {
-        if (isSelectedEnv) {
-            return spanBold(currEnv)
+    fun buildRelevantSortedEnvironments(envsSupplier: EnvironmentsSupplier): List<String> {
+        val builtEnvs = ArrayList<String>()
+
+        var mineLocalEnv = ""
+
+        for (currEnv in envsSupplier.getEnvironments()) {
+            if (isEnvironmentLocal(currEnv)) {
+                if (isLocalEnvironmentMine(currEnv)) {
+                    mineLocalEnv = currEnv
+                }
+                continue
+            } else {
+                builtEnvs.add(currEnv)
+            }
         }
-        return span(currEnv)
+
+        builtEnvs.sortWith(String.CASE_INSENSITIVE_ORDER)
+        if (mineLocalEnv.isNotBlank()) {
+            builtEnvs.add(0, mineLocalEnv)
+        }
+        return builtEnvs
     }
+
+    fun buildLinkText(currEnv: String, isSelectedEnv: Boolean): String {
+        var txtValue = currEnv
+        if (isLocalEnvironmentMine(currEnv)) {
+            txtValue = "LOCAL"
+        }
+        if (isSelectedEnv) {
+            return spanBold(txtValue)
+        }
+        return span(txtValue)
+    }
+}
+
+fun isEnvironmentLocal(environment: String): Boolean {
+    return environment.endsWith("[local]", true)
+}
+
+fun isLocalEnvironmentMine(environment: String): Boolean {
+    val localHostname = CommonUtils.getLocalHostname()
+    return environment.startsWith(localHostname, true)
 }
 
 class SingleEnvPanel : JBPanel<SingleEnvPanel>() {

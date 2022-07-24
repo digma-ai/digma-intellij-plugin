13a0f8215e67dcdd3a2ee7b151220075bf3d9353
arik-dig <asher@digma.ai>
2022-07-24 12:08:20 +0300

    AnalyticsProvider add query of UsageStatus
    Signed-off-by: arik-dig <asher@digma.ai>

--- analytics-provider/src/main/java/org/digma/intellij/plugin/analytics/AnalyticsProvider.java
+++ analytics-provider/src/main/java/org/digma/intellij/plugin/analytics/AnalyticsProvider.java
@@ -6,6 +6,8 @@ import org.digma.intellij.plugin.model.rest.insights.CodeObjectInsight;
 import org.digma.intellij.plugin.model.rest.insights.InsightsRequest;
 import org.digma.intellij.plugin.model.rest.summary.CodeObjectSummary;
 import org.digma.intellij.plugin.model.rest.summary.CodeObjectSummaryRequest;
+import org.digma.intellij.plugin.model.rest.usage.UsageStatusRequest;
+import org.digma.intellij.plugin.model.rest.usage.UsageStatusResult;
 
 import java.io.Closeable;
 import java.util.List;
@@ -21,5 +23,7 @@ public interface AnalyticsProvider extends Closeable {
     List<CodeObjectError> getErrorsOfCodeObject(String environment, String codeObjectId);
 
     CodeObjectErrorDetails getCodeObjectErrorDetails(String errorSourceId);
+
+    UsageStatusResult getUsageStatus(UsageStatusRequest usageStatusRequest);
 
 }

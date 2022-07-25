41fcbf993a826181ee1e7c6db67864f368df7c5e
arik-dig <asher@digma.ai>
2022-07-25 13:15:39 +0300

    cosmetic
    Signed-off-by: arik-dig <asher@digma.ai>

--- model/src/main/kotlin/org/digma/intellij/plugin/ui/model/errors/ErrorsModel.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/ui/model/errors/ErrorsModel.kt
@@ -1,18 +1,22 @@
 package org.digma.intellij.plugin.ui.model.errors
 
-import org.digma.intellij.plugin.model.Models
+import org.digma.intellij.plugin.model.Models.Empties.EmptyUsageStatusResult
 import org.digma.intellij.plugin.model.rest.errors.CodeObjectError
 import org.digma.intellij.plugin.model.rest.usage.UsageStatusResult
-import org.digma.intellij.plugin.ui.model.*
+import org.digma.intellij.plugin.ui.model.DocumentScope
+import org.digma.intellij.plugin.ui.model.EmptyScope
+import org.digma.intellij.plugin.ui.model.MethodScope
+import org.digma.intellij.plugin.ui.model.PanelModel
+import org.digma.intellij.plugin.ui.model.Scope
 import org.digma.intellij.plugin.ui.model.listview.ListViewItem
 import java.lang.Integer.max
-import java.util.*
+import java.util.Collections
 
 class ErrorsModel : PanelModel {
 
     var errorsCount: Int = 0
     var listViewItems: List<ListViewItem<CodeObjectError>> = Collections.emptyList()
-    var usageStatusResult: UsageStatusResult = Models.Empties.EmptyUsageStatusResult //TODO: fill me
+    var usageStatusResult: UsageStatusResult = EmptyUsageStatusResult
     var scope: Scope = EmptyScope("")
     var errorDetails: ErrorDetailsModel = ErrorDetailsModel()
     var card: ErrorsTabCard = ErrorsTabCard.ERRORS_LIST

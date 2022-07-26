77fed76f0ad767a1d017f6b0b381df19a94c12c1
shalom <shalom938@gmail.com>
2022-07-26 13:39:08 +0300

    description in plugin.xml
    Signed-off-by: shalom <shalom938@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,11 +105,11 @@ tasks {
 //        )
 
         // Get the latest available change notes from the changelog file
-        changeNotes.set(provider {
-            changelog.run {
-                getOrNull(properties("pluginVersion")) ?: getLatest()
-            }.toHTML()
-        })
+//        changeNotes.set(provider {
+//            changelog.run {
+//                getOrNull(properties("pluginVersion")) ?: getLatest()
+//            }.toHTML()
+//        })
     }
 
     // Configure UI tests plugin

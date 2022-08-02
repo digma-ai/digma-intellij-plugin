aee435e0c363a883aefa0d2da1f4913c0bf60337
arik-dig <asher@digma.ai>
2022-08-02 15:45:33 +0300

    simpler defs

--- model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/SpanHistogramQuery.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/SpanHistogramQuery.kt
@@ -1,16 +1,10 @@
 package org.digma.intellij.plugin.model.rest.insights
 
 import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import java.beans.ConstructorProperties
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class SpanHistogramQuery @JsonCreator(mode = JsonCreator.Mode.PROPERTIES) @ConstructorProperties(
-    "environment",
-    "spanName",
-    "instrumentationLibrary",
-    "codeObjectId"
-) constructor(
+data class SpanHistogramQuery
+@JsonCreator
+constructor(
     val environment: String,
     val spanName: String,
     val instrumentationLibrary: String,

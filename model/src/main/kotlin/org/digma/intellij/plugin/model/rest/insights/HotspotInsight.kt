a90d3017c971066514d579decf66a2ca0705009b
shalom <shalom938@gmail.com>
2022-08-17 01:41:28 +0300

    fix hotspot insight data class
    Signed-off-by: shalom <shalom938@gmail.com>

--- model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/HotspotInsight.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/HotspotInsight.kt
@@ -1,13 +1,10 @@
 package org.digma.intellij.plugin.model.rest.insights
 
 import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import org.digma.intellij.plugin.model.InsightType
 import java.beans.ConstructorProperties
 
-@JsonIgnoreProperties("updatedAt")
 data class HotspotInsight
-
 @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
 @ConstructorProperties("codeObjectId", "score")
 constructor(

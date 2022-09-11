16dacc8245a3d6806af5f32a8df4060799101e4a
arik-dig <asher@digma.ai>
2022-09-11 18:00:35 +0300

    showing the slowEndpointInfo(s) as in VSCode

--- src/main/kotlin/org/digma/intellij/plugin/ui/list/insights/SlowestSpansPanel.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/list/insights/SlowestSpansPanel.kt
@@ -5,7 +5,9 @@ import com.intellij.ui.components.ActionLink
 import com.intellij.ui.components.JBLabel
 import com.intellij.util.ui.JBUI
 import org.digma.intellij.plugin.document.CodeObjectsUtil
+import org.digma.intellij.plugin.model.rest.insights.EndpointSchema
 import org.digma.intellij.plugin.model.rest.insights.Percentile
+import org.digma.intellij.plugin.model.rest.insights.SlowEndpointInfo
 import org.digma.intellij.plugin.model.rest.insights.SlowSpanInfo
 import org.digma.intellij.plugin.model.rest.insights.SlowestSpansInsight
 import org.digma.intellij.plugin.model.rest.insights.SpanSlowEndpointsInsight
@@ -61,7 +63,25 @@ fun slowestSpansPanel(project: Project, insight: SlowestSpansInsight, moreData: 
 
 fun spanSlowEndpointsPanel(project: Project, insight: SpanSlowEndpointsInsight, panelsLayoutHelper: PanelsLayoutHelper): JPanel {
     val endpointsListPanel = JPanel()
-    //TODO: fill data from insight itself
+    endpointsListPanel.layout = GridLayout(insight.slowEndpoints.size, 1, 0, 3)
+    endpointsListPanel.border = JBUI.Borders.empty()
+    endpointsListPanel.isOpaque = false
+
+    insight.slowEndpoints.forEach {slowEndpointInfo ->
+        val currContainerPanel = JPanel(GridLayout(2, 1, 0, 3))
+        endpointsListPanel.border = JBUI.Borders.empty()
+        currContainerPanel.isOpaque = false
+
+        val routeInfo = EndpointSchema.getRouteInfo(slowEndpointInfo.endpointInfo.route)
+        val shortRouteName =  routeInfo.shortName
+
+        val line1 = JBLabel(asHtml("${slowEndpointInfo.endpointInfo.serviceName}: <b>$shortRouteName</b>"))
+        val line2 = JBLabel(asHtml(descriptionOf(slowEndpointInfo)))
+
+        currContainerPanel.add(line1)
+        currContainerPanel.add(line2)
+        endpointsListPanel.add(currContainerPanel);
+    }
 
     return createInsightPanel(
         "Bottleneck",
@@ -72,6 +92,18 @@ fun spanSlowEndpointsPanel(project: Project, insight: SpanSlowEndpointsInsight, 
         panelsLayoutHelper)
 }
 
+fun descriptionOf(sei: SlowEndpointInfo): String {
+    if (sei.p95.fraction > 0) {
+        return "Up to ~${percentageForDisplaySlowSpanInfo(sei.p95)} of entire of the entire request time ${durationText(sei.p95)}"
+    }
+
+    return "Up to ~${percentageForDisplaySlowSpanInfo(sei.p50)} of entire of the entire request time ${durationText(sei.p50)}"
+}
+
+fun durationText(percentile: Percentile): String {
+    return "${percentile.maxDuration.value}${percentile.maxDuration.unit}"
+}
+
 fun descriptionOf(span: SlowSpanInfo): String {
     if (span.p50.fraction > 0.4)
         return "50% of the users by up to ${span.p50.maxDuration.value}${span.p50.maxDuration.unit}"
@@ -80,6 +112,11 @@ fun descriptionOf(span: SlowSpanInfo): String {
         return "5% of the users by up to ${span.p95.maxDuration.value}${span.p95.maxDuration.unit}"
 
     return "1% of the users by up to ${span.p99.maxDuration.value}${span.p99.maxDuration.unit}"
+}
+
+private fun percentageForDisplaySlowSpanInfo(percentile: Percentile): String {
+    val decimal = BigDecimal(percentile.fraction * 100).setScale(3, RoundingMode.HALF_DOWN)
+    return decimal.toPlainString()
 }
 
 fun genToolTip(span: SlowSpanInfo): String {

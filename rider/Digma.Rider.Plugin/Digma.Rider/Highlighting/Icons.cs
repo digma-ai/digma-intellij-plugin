00bb1bdae204e72647384e26eedc305d685dab28
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2022-09-14 09:11:04 +0300

    add icon to error hotspot (#149)
    Signed-off-by: shalom <shalom938@gmail.com>
    
    Signed-off-by: shalom <shalom938@gmail.com>

---
+++ rider/Digma.Rider.Plugin/Digma.Rider/Highlighting/Icons.cs
@@ -0,0 +1,9 @@
+using JetBrains.RdBackend.Common.Features.Icons;
+
+namespace Digma.Rider.Highlighting
+{
+    public class Icons
+    {
+        public static FrontendIconId ExclMark = new FrontendIconId("icons/exclMark.svg");
+    }
+}

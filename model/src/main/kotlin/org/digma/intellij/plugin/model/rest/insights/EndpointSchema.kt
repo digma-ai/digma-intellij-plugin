5fc51373ae2148254eecf7bc3dd5022dc46889d2
MykolaYaremchuk <myaremchuk@digma.ai>
2022-12-08 18:52:11 +0200

    Removed group labels (#134)

--- model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/EndpointSchema.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/EndpointSchema.kt
@@ -2,7 +2,7 @@ package org.digma.intellij.plugin.model.rest.insights
 
 import javax.swing.Icon
 
-class GroupViewModel(val titleText: String, val labelText: String, val icon: Icon)
+class GroupViewModel(val labelText: String, val icon: Icon)
 
 class RouteInfo(val shortName: String, val schema: String)
 
@@ -15,14 +15,15 @@ class EndpointSchema {
 
         @JvmStatic
         fun getRouteInfo(fullRouteName: String): RouteInfo {
-            val schema = getSchema(fullRouteName);
-            return RouteInfo(removeSchema(fullRouteName, schema), schema);
+            val schema = getSchema(fullRouteName)
+            return RouteInfo(removeSchema(fullRouteName, schema), schema)
         }
 
         @JvmStatic
         private fun isOfType(fullRouteName: String, schema: String): Boolean{
-            return fullRouteName.startsWith("$schema:");
+            return fullRouteName.startsWith("$schema:")
         }
+
         @JvmStatic
         private fun getSchema(fullRouteName: String): String{
             if (isOfType(fullRouteName, HTTP_SCHEMA)) {
@@ -34,31 +35,31 @@ class EndpointSchema {
             if (isOfType(fullRouteName, CONSUMER_SCHEMA)) {
                 return CONSUMER_SCHEMA
             }
-            return "";
+            return ""
         }
 
         @JvmStatic
         private fun removeSchema(fullRouteName: String, schema: String): String{
             if(schema == ""){
-                return fullRouteName;
+                return fullRouteName
             }
-            return fullRouteName.replace("$schema:", "");
+            return fullRouteName.replace("$schema:", "")
         }
 
         @JvmStatic
         fun adjustHttpRouteIfNeeded(endpointInsight: EndpointInsight) {
             val origValue = endpointInsight.route
             if (isOfType(origValue, HTTP_SCHEMA)) {
-                return;
+                return
             }
             if (isOfType(origValue, RPC_SCHEMA)) {
-                return;
+                return
             }
             if (isOfType(origValue, CONSUMER_SCHEMA)) {
-                return;
+                return
             }
             // default behaviour, to be backward compatible, where did not have the scheme part of the route, so adding it as HTTP one
-            endpointInsight.route = HTTP_SCHEMA +":"+ origValue;
+            endpointInsight.route = "$HTTP_SCHEMA:$origValue"
         }
     }
 }

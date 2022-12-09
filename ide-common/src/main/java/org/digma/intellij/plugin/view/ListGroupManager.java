b338be297828af885044a4ff8b6a9fc27d829093
MykolaYaremchuk <myaremchuk@digma.ai>
2022-12-09 15:27:33 +0200

    Merge span and REST call into same group (#123)

--- ide-common/src/main/java/org/digma/intellij/plugin/view/ListGroupManager.java
+++ ide-common/src/main/java/org/digma/intellij/plugin/view/ListGroupManager.java
@@ -5,6 +5,7 @@ import org.digma.intellij.plugin.ui.model.listview.GroupListViewItem;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 public class ListGroupManager {
@@ -12,7 +13,12 @@ public class ListGroupManager {
     private final Map<String, GroupListViewItem> groups = new HashMap<>();
 
     public GroupListViewItem getOrCreateGroup(String groupKey, Supplier<GroupListViewItem> supplier) {
-        return groups.computeIfAbsent(groupKey, key -> supplier.get());
+        Optional<String> existingMatchingGroupKey = groups.keySet()
+                .stream()
+        //FYI: Temporary fix because from BE we receive some insights with groupId = "epHTTP:POST Transfer/TransferFunds" and some insights with groupId = "HTTP POST Transfer/TransferFunds" but all these items should be displayed on the same group on UI
+                .filter(s -> s.endsWith(groupKey.replace("HTTP ", "")))
+                .findFirst();
+        return groups.computeIfAbsent(existingMatchingGroupKey.orElse(groupKey), key -> supplier.get());
     }
 
     public Collection<GroupListViewItem> getGroupItems() {

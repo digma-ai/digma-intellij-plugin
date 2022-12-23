bc70f73206983135cdb548735a238433fa7eb63b
shalom <shalom938@gmail.com>
2022-12-23 14:16:11 +0200

    improve document change listener

--- ide-common/src/main/java/org/digma/intellij/plugin/editor/DocumentChangeListener.java
+++ ide-common/src/main/java/org/digma/intellij/plugin/editor/DocumentChangeListener.java
@@ -1,11 +1,14 @@
 package org.digma.intellij.plugin.editor;
 
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.event.DocumentListener;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.IndexNotReadyException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
@@ -15,8 +18,9 @@ import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiInvalidElementAccessException;
 import com.intellij.util.Alarm;
 import com.intellij.util.AlarmFactory;
+import com.intellij.util.RunnableCallable;
+import com.intellij.util.concurrency.NonUrgentExecutor;
 import com.intellij.util.indexing.FileBasedIndex;
-import org.digma.intellij.plugin.common.Backgroundable;
 import org.digma.intellij.plugin.document.DocumentInfoService;
 import org.digma.intellij.plugin.index.DocumentInfoIndex;
 import org.digma.intellij.plugin.log.Log;
@@ -97,18 +101,35 @@ class DocumentChangeListener {
                 documentChangeAlarm.cancelAllRequests();
                 documentChangeAlarm.addRequest(() ->
                 {
-                    //this code is always executed in smart mode because the listener is installed only in smart mode
-                    PsiFile fileToQuery = PsiDocumentManager.getInstance(project).getPsiFile(event.getDocument());
+                    //this code is always executed in smart mode because the document listener is installed only in smart mode
+                    PsiFile changedPsiFile = PsiDocumentManager.getInstance(project).getPsiFile(event.getDocument());
                     //probably should never happen
-                    if (fileToQuery == null) {
+                    if (changedPsiFile == null) {
                         return;
                     }
-                    try {
-                        Log.log(LOGGER::debug, "Processing documentChanged event for {}", fileToQuery.getVirtualFile());
-                        processDocumentChanged(editor, fileToQuery);
-                    } catch (PsiInvalidElementAccessException e) {
-                        Log.debugWithException(LOGGER, e, "exception while processing file: {}, {}", fileToQuery.getVirtualFile(), e.getMessage());
-                    }
+
+                    ReadAction.nonBlocking(new RunnableCallable(() -> {
+                        try {
+                            Log.log(LOGGER::debug, "Processing documentChanged event for {}", changedPsiFile.getVirtualFile());
+                            processDocumentChanged(changedPsiFile);
+                        } catch (PsiInvalidElementAccessException e) {
+                            Log.debugWithException(LOGGER, e, "exception while processing documentChanged event for file: {}, {}", changedPsiFile.getVirtualFile(), e.getMessage());
+                        }
+                    })).inSmartMode(project).withDocumentsCommitted(project).finishOnUiThread(ModalityState.defaultModalityState(), unused -> {
+
+                        var selectedTextEditor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+                        if (selectedTextEditor != null) {
+                            PsiFile selectedPsiFile = PsiDocumentManager.getInstance(project).getPsiFile(selectedTextEditor.getDocument());
+                            //if the selected editor is still the file that was changed in this event then call contextChanged
+                            // otherwise do nothing
+                            if (selectedPsiFile != null && selectedPsiFile.equals(changedPsiFile)) {
+                                LanguageService languageService = LanguageServiceLocator.getInstance(project).locate(selectedPsiFile.getLanguage());
+                                MethodUnderCaret methodUnderCaret = languageService.detectMethodUnderCaret(project, selectedPsiFile, selectedTextEditor.getCaretModel().getOffset());
+                                caretContextService.contextChanged(methodUnderCaret);
+                            }
+                        }
+
+                    }).submit(NonUrgentExecutor.getInstance());
 
                 }, 500);
 
@@ -117,64 +138,44 @@ class DocumentChangeListener {
     }
 
 
-    private void processDocumentChanged(@NotNull Editor editor, @NotNull PsiFile psiFile) {
+    private void processDocumentChanged(@NotNull PsiFile psiFile) {
 
         if (project.isDisposed()) {
             return;
         }
 
+        LanguageService languageService = LanguageServiceLocator.getInstance(project).locate(psiFile.getLanguage());
 
-        PsiDocumentManager.getInstance(project).performLaterWhenAllCommitted(() -> {
+        DocumentInfo documentInfo;
+        try {
+            Map<Integer, DocumentInfo> documentInfoMap =
+                    FileBasedIndex.getInstance().getFileData(DocumentInfoIndex.DOCUMENT_INFO_INDEX_ID, psiFile.getVirtualFile(), project);
+            //there is only one DocumentInfo per file in the index.
+            //all relevant files must be indexed, so if we are here then DocumentInfo must be found in the index is ready,
+            // or we have a mistake somewhere else. java interfaces,enums and annotations are indexed but the DocumentInfo
+            // object is empty of methods, that's because currently we have no way to exclude those types from indexing.
+            documentInfo = documentInfoMap.values().stream().findFirst().orElse(null);
 
-            LanguageService languageService = LanguageServiceLocator.getInstance(project).locate(psiFile.getLanguage());
-
-            DocumentInfo documentInfo;
-            try {
-                Map<Integer, DocumentInfo> documentInfoMap =
-                        FileBasedIndex.getInstance().getFileData(DocumentInfoIndex.DOCUMENT_INFO_INDEX_ID, psiFile.getVirtualFile(), project);
-                //there is only one DocumentInfo per file in the index.
-                //all relevant files must be indexed, so if we are here then DocumentInfo must be found in the index is ready,
-                // or we have a mistake somewhere else. java interfaces,enums and annotations are indexed but the DocumentInfo
-                // object is empty of methods, that's because currently we have no way to exclude those types from indexing.
-                documentInfo = documentInfoMap.values().stream().findFirst().orElse(null);
-
-                //usually we should find the document info in the index. on extreme cases, maybe is the index is corrupted
-                // the document info will not be found, try again to build it
-                if (documentInfo == null) {
-                    documentInfo = languageService.buildDocumentInfo(psiFile);
-                }
-
-            } catch (IndexNotReadyException e) {
-                //IndexNotReadyException will be thrown on dumb mode, when indexing is still in progress.
-                //usually it should not happen because the listener is installed only in smart mode.
+            //usually we should find the document info in the index. on extreme cases, maybe if the index is corrupted
+            // the document info will not be found, try again to build it
+            if (documentInfo == null) {
                 documentInfo = languageService.buildDocumentInfo(psiFile);
             }
 
-            if (documentInfo == null) {
-                Log.log(LOGGER::error, "Could not find DocumentInfo for file {}", psiFile.getVirtualFile());
-                throw new DocumentInfoIndexNotFoundException("Could not find DocumentInfo index for " + psiFile.getVirtualFile());
-            }
-            Log.log(LOGGER::debug, "Found DocumentInfo index for {},'{}'", psiFile.getVirtualFile(), documentInfo);
+        } catch (IndexNotReadyException e) {
+            //IndexNotReadyException will be thrown on dumb mode, when indexing is still in progress.
+            //usually it should not happen because the document listener is installed only in smart mode.
+            documentInfo = languageService.buildDocumentInfo(psiFile);
+        }
 
-            MethodUnderCaret methodUnderCaret = languageService.detectMethodUnderCaret(project, psiFile, editor.getCaretModel().getOffset());
+        if (documentInfo == null) {
+            Log.log(LOGGER::error, "Could not find DocumentInfo for file {}", psiFile.getVirtualFile());
+            throw new DocumentInfoIndexNotFoundException("Could not find DocumentInfo index for " + psiFile.getVirtualFile());
+        }
+        Log.log(LOGGER::debug, "Found DocumentInfo index for {},'{}'", psiFile.getVirtualFile(), documentInfo);
 
-            update(languageService, psiFile, documentInfo, methodUnderCaret);
-
-        });
-    }
-
-
-    private void update(@NotNull LanguageService languageService, @NotNull PsiFile psiFile, DocumentInfo documentInfo, MethodUnderCaret methodUnderCaret) {
-        Backgroundable.ensureBackground(project, "Document changed", () -> {
-            //documentInfoService will update the discovery code objects, refresh backend data and call some event
-            //so that code lens will be installed.
-            //contextChanged will update the UI and must run after documentInfoService.addCodeObjects is finished
-            //enrichDocumentInfo is meant mainly to discover spans. the DocumentInfoIndex can
-            // not discover spans because there is no reference resolving during file based index.
-            languageService.enrichDocumentInfo(documentInfo, psiFile);
-            documentInfoService.addCodeObjects(psiFile, documentInfo);
-            caretContextService.contextChanged(methodUnderCaret);
-        });
+        languageService.enrichDocumentInfo(documentInfo, psiFile);
+        documentInfoService.addCodeObjects(psiFile, documentInfo);
     }
 
 

48fb93f56ec44becdda8d441e17495923f05e5a5
arik-dig <asher@digma.ai>
2023-01-19 16:09:37 +0200

    Feature/JAX-RS - refer to class and method hierarchies when searching for annotations #277

--- java/src/main/kotlin/org/digma/intellij/plugin/idea/psi/java/JavaPsiUtils.kt
+++ java/src/main/kotlin/org/digma/intellij/plugin/idea/psi/java/JavaPsiUtils.kt
@@ -1,7 +1,12 @@
 package org.digma.intellij.plugin.idea.psi.java
 
+import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiJavaFile
+import com.intellij.psi.PsiMethod
 import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
 
 class JavaPsiUtils {
 
@@ -33,5 +38,50 @@ class JavaPsiUtils {
                 return prevCLass
             }
         }
+
+        @JvmStatic
+        @NotNull
+        fun getClassesWithin(psiFile: PsiFile): List<PsiClass> {
+            //it must be a PsiJavaFile so casting should be ok
+            val psiJavaFile = psiFile as PsiJavaFile
+            return psiJavaFile.classes.asList()
+        }
+
+        @JvmStatic
+        @Nullable
+        fun findNearestAnnotation(psiMethod: PsiMethod, annotationFqn: String): PsiAnnotation? {
+            val annotClass = psiMethod.getAnnotation(annotationFqn)
+            if (annotClass != null) {
+                return annotClass
+            }
+
+            val superMethods = psiMethod.findSuperMethods(false)
+            superMethods.forEach {
+                val theAnnotation = it.getAnnotation(annotationFqn)
+                if (theAnnotation != null) {
+                    return theAnnotation
+                }
+            }
+            return null
+        }
+
+        @JvmStatic
+        @Nullable
+        fun findNearestAnnotation(psiClass: PsiClass, annotationFqn: String): PsiAnnotation? {
+            val annotClass = psiClass.getAnnotation(annotationFqn)
+            if (annotClass != null) {
+                return annotClass
+            }
+
+            val superClasses = psiClass.supers
+            superClasses.forEach {
+                val theAnnotation = it.getAnnotation(annotationFqn)
+                if (theAnnotation != null) {
+                    return theAnnotation
+                }
+            }
+            return null
+        }
+
     }
 }

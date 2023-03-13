2ebb93b3edeaa82503cfb616aed638123368d73f
shalom <shalom938@gmail.com>
2023-03-13 14:03:05 +0200

    fix comments

--- rider/src/main/kotlin/org/digma/intellij/plugin/rider/protocol/protocol.kt
+++ rider/src/main/kotlin/org/digma/intellij/plugin/rider/protocol/protocol.kt
@@ -19,19 +19,19 @@ when calling in the java side to PsiFile.getVirtualFile().getUrl() it will retur
 when a document is loaded we use the uri from resharper as key in DocumentInfoService, if we later try to find a
 DocumentInfo by calling PsiFile.getVirtualFile().getUrl() the document info will not be found because that call will
 return a file schema with 3 slashes.
-Another place we use the uri is in ElementUnderCaretDetector, the MethodUnderCaretEvent.fileUri will have 2 slashes.
 
-the solution here is to always normalize the uri that comes from the resharper side to a uri as comes in the jvm side.
-so actually every object that passes from resharper to the jvm and has a uri that was taken from
+the solution here is to always normalize the uri that comes from the resharper side to an uri as comes in the jvm side.
+so actually every object that passes from resharper to the jvm and has an uri that was taken from
 IPsiSourceFile.GetLocation().ToUri().ToString() needs to be normalized.
-not all file uri in all objects is used, the main one that causes issues if the MethodUnderCaretEvent.fileUri,
+not all file uri in all objects is used, the main one that causes issues if the RideDocumentInfo.fileUri,
 but we normalize all the objects.
 
 Note that PsiUtils.uriToPsiFile(fileUri,project) will always work , with 2 or with 3 slashes.
 the issue is only when we use the uri as key in our own code, opening a file with intellij utils will
 work regardless of the number of slashes. so for example the uris for span navigation returned from
-CodeObjectHost.findWorkspaceUrisForSpanIds or CodeObjectHost.findWorkspaceUrisForCodeObjectIds will also contain
-2 slashes, but we don't use these as keys or to find DocumentInfo, they are used to open files which will always work.
+findWorkspaceUrisForSpanIds or findWorkspaceUrisForCodeObjectIdsForErrorStackTrace or
+findWorkspaceUrisForMethodCodeObjectIds will also contain 2 slashes, but we don't use these
+as keys or to find DocumentInfo, they are used to open files which will always work.
 
 
  */
@@ -41,9 +41,10 @@ fun normalizeFileUri(fileUri: String,project:Project): String {
         return fileUri
     }
 
-    //when this method is called from ElementUnderCaretDetector.toModel the file uri may be a non supported file
-    //like json or a non-user file where there is no psi file. so in that case catch the exception and just return
-    //the original file uri
+    //the way to normalize is find file by uri which will always work and recreate a uri again.
+    // PsiUtils.uriToPsiFile will always work , with 2 or 3 slashes,and PsiUtils.psiFileToUri will return
+    // 3 slashes. catch any exception and just return the original, that should not happen unless we have a
+    // bug somewhere
     return try {
         val psiFile = PsiUtils.uriToPsiFile(fileUri, project)
         PsiUtils.psiFileToUri(psiFile)

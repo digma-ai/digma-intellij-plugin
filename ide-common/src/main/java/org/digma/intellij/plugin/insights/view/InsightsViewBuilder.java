783da3655236e5311dca3d833054d17796bd3c1e
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-03-22 15:54:28 +0200

    Feature/no data yet panel per span (#408)

--- ide-common/src/main/java/org/digma/intellij/plugin/insights/view/InsightsViewBuilder.java
+++ ide-common/src/main/java/org/digma/intellij/plugin/insights/view/InsightsViewBuilder.java
@@ -1,10 +1,17 @@
 package org.digma.intellij.plugin.insights.view;
 
+import com.google.common.collect.Sets;
 import com.intellij.openapi.project.Project;
 import org.digma.intellij.plugin.model.discovery.MethodInfo;
+import org.digma.intellij.plugin.model.discovery.SpanInfo;
 import org.digma.intellij.plugin.model.rest.insights.CodeObjectInsight;
 import org.digma.intellij.plugin.model.rest.insights.EndpointInsight;
 import org.digma.intellij.plugin.model.rest.insights.EndpointSchema;
+import org.digma.intellij.plugin.model.rest.insights.SpanInsight;
+import org.digma.intellij.plugin.ui.model.insights.InsightGroupListViewItem;
+import org.digma.intellij.plugin.ui.model.insights.InsightGroupType;
+import org.digma.intellij.plugin.ui.model.insights.NoDataYet;
+import org.digma.intellij.plugin.ui.model.listview.GroupListViewItem;
 import org.digma.intellij.plugin.ui.model.listview.ListViewItem;
 import org.digma.intellij.plugin.view.ListViewBuilder;
 import org.digma.intellij.plugin.view.ListViewItemBuilder;
@@ -13,6 +20,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class InsightsViewBuilder extends ListViewBuilder {
 
@@ -32,13 +41,47 @@ public class InsightsViewBuilder extends ListViewBuilder {
 
         codeObjectInsights.forEach(insight -> {
             final ListViewItemBuilder<CodeObjectInsight> builder = (ListViewItemBuilder<CodeObjectInsight>) buildersHolder.getBuilder(insight.getType());
-            final List<ListViewItem<?>> insightListItems = builder.build(project,methodInfo, insight, groupManager);
+            final List<ListViewItem<?>> insightListItems = builder.build(project, methodInfo, insight, groupManager);
             allItems.addAll(insightListItems);
         });
+
+        Set<String> spansThatHaveNoInsight = findLocalSpansThatHaveNoInsights(methodInfo, codeObjectInsights);
+        if (!spansThatHaveNoInsight.isEmpty()) {
+            buildItemsForNoDataYet(spansThatHaveNoInsight);
+        }
 
         allItems.addAll(groupManager.getGroupItems());
 
         return sortDistinct(allItems);
+    }
+
+    protected static Set<String> findLocalSpansThatHaveNoInsights(@NotNull MethodInfo methodInfo, List<? extends CodeObjectInsight> codeObjectInsights) {
+
+        Set<String> spansOfMethod = methodInfo.getSpans().stream()
+                .map(SpanInfo::getName)
+                .collect(Collectors.toSet());
+
+        Set<String> spansThatHaveInsights = codeObjectInsights.stream()
+                .filter(it -> it instanceof SpanInsight)
+                .map(it -> (SpanInsight) it)
+                .map(SpanInsight::spanName)// span name without instrumentation library
+                .collect(Collectors.toSet());
+
+        // spansOfMethod minus spansThatHaveInsights
+        Set<String> spansThatHaveNoInsights = Sets.difference(spansOfMethod, spansThatHaveInsights).immutableCopy();
+
+        return spansThatHaveNoInsights;
+    }
+
+    protected void buildItemsForNoDataYet(Set<String> spansThatHaveNoInsight) {
+        for (String currSpanName : spansThatHaveNoInsight) {
+            String groupId = currSpanName;
+            GroupListViewItem theGroup = groupManager.getOrCreateGroup(
+                    groupId, () -> new InsightGroupListViewItem(currSpanName, InsightGroupType.Span, ""));
+
+            ListViewItem<NoDataYet> itemOfNoDataYet = new ListViewItem<>(new NoDataYet(), 222);
+            theGroup.addItem(itemOfNoDataYet);
+        }
     }
 
     protected void adjustToHttpIfNeeded(final List<? extends CodeObjectInsight> codeObjectInsights) {

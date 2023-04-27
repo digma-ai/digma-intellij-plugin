e0d682b727386873d981991958b35702c14a6dab
Asaf Chen <achen@digma.ai>
2023-04-27 15:03:01 +0300

    Notifiy posthog when use uninstall figma plugin

---
+++ ide-common/src/main/kotlin/org/digma/intellij/plugin/posthog/PluginActivityMonitor.kt
@@ -0,0 +1,40 @@
+package org.digma.intellij.plugin.posthog
+
+import com.intellij.ide.plugins.IdeaPluginDescriptor
+import com.intellij.ide.plugins.PluginStateListener
+import com.intellij.ide.plugins.PluginStateManager
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+import org.digma.intellij.plugin.PluginId
+
+class PluginActivityMonitor(private val project: Project) : PluginStateListener, Disposable {
+    companion object {
+        private val LOGGER = Logger.getInstance(PluginActivityMonitor::class.java)
+
+        @JvmStatic
+        fun loadInstance(project: Project) {
+            project.getService(PluginActivityMonitor::class.java)
+        }
+    }
+
+    init {
+        PluginStateManager.addStateListener(this)
+    }
+
+    override fun uninstall(descriptor: IdeaPluginDescriptor) {
+        if(descriptor.pluginId.idString == PluginId.PLUGIN_ID){
+            ActivityMonitor.getInstance(project).registerPluginUninstalled()
+        }
+
+        super.uninstall(descriptor)
+    }
+
+    override fun install(descriptor: IdeaPluginDescriptor) {
+        // nothing
+    }
+
+    override fun dispose() {
+        PluginStateManager.removeStateListener(this)
+    }
+}

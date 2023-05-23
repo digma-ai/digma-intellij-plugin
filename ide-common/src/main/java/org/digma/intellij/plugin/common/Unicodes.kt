9f343d66325f5fe3ccb884064e24ea3fe6fbce3b
Roni Dover <roni.dover@gmail.com>
2023-05-23 15:50:16 -0700

    updating unicode symbol and tooltips

--- ide-common/src/main/java/org/digma/intellij/plugin/common/Unicodes.kt
+++ ide-common/src/main/java/org/digma/intellij/plugin/common/Unicodes.kt
@@ -1,12 +1,10 @@
 package org.digma.intellij.plugin.common
 
-import java.awt.Font
-
 class Unicodes {
     companion object {
         // Unicode Character 'LARGE GREEN CIRCLE' U+1F7E2
-        // https://www.fileformat.info/info/unicode/char/1f7e2/index.htm
+        // https://www.fileformat.info/info/unicode/char/23fa/index.htm
         @JvmStatic
-        val GREEN_CIRCLE: String = "\uD83D\uDFE2" // 🟢
+        val LIVE_CIRCLE: String = "\u23FA" //
     }
 }

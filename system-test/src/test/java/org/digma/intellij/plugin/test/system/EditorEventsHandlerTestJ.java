4d91443d3d6fa909167a81f24bdc23b6cb2a229c
Yoram <yoram@ignissoft.com>
2023-05-23 10:02:31 +0300

    setPluginXml - waiting for answers from IntelliJ slack

--- system-test/src/test/java/org/digma/intellij/plugin/test/system/EditorEventsHandlerTestJ.java
+++ system-test/src/test/java/org/digma/intellij/plugin/test/system/EditorEventsHandlerTestJ.java
@@ -1,12 +1,11 @@
 package org.digma.intellij.plugin.test.system;
 
-import com.intellij.openapi.components.ComponentManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import com.intellij.testFramework.LightVirtualFile;
-import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import com.intellij.testFramework.TestDataFile;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import org.digma.intellij.plugin.editor.EditorEventsHandler;
 import org.digma.intellij.plugin.log.Log;
@@ -15,15 +14,30 @@ import org.digma.intellij.plugin.psi.LanguageService;
 import org.digma.intellij.plugin.psi.LanguageServiceLocator;
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.nio.file.Paths;
 
 public class EditorEventsHandlerTestJ extends LightJavaCodeInsightFixtureTestCase {
 
     private static final Logger LOGGER = Logger.getInstance(EditorEventsHandler.class);
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/resources";
+    }
+
+    protected void setPluginXml(@TestDataFile String pluginXml) {
+        final VirtualFile file = myFixture.copyFileToProject(pluginXml, "META-INF/plugin.xml");
+        final PluginBuildConfiguration pluginBuildConfiguration = getPluginBuildConfiguration();
+        pluginBuildConfiguration.setPluginXmlFromVirtualFile(file);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
 
     @Test
     public void testPsiFileNotEmpty() {
+        setPluginXml("plugin.xml");
         myFixture.setTestDataPath(Paths.get("src/test/resources").toAbsolutePath().toString());
         PsiFile psiFile = myFixture.configureByFile("EditorEventsHandler.java");
         Assert.assertNotNull("PsiFile should not be null", psiFile);
@@ -31,8 +45,7 @@ public class EditorEventsHandlerTestJ extends LightJavaCodeInsightFixtureTestCas
 
         VirtualFile newFile = new LightVirtualFile("EditorEventsHandler.java");
         Project project = getProject();
-        ComponentManager componentManager = project.getComponent(ComponentManager.class);
-        LanguageServiceLocator languageServiceLocator = componentManager.getService(LanguageServiceLocator.class);
+        LanguageServiceLocator languageServiceLocator = project.getService(LanguageServiceLocator.class);
         LanguageService languageService = languageServiceLocator.locate(psiFile.getLanguage());
         Log.log(LOGGER::debug, "Found language service {} for :{}",languageService, newFile);
 

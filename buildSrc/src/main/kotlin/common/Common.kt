ce03d25e4faa73f46daa239d3f6ee580022ba76c
shalom <shalom938@gmail.com>
2023-05-25 01:42:59 +0300

    start support for 2023.2

--- buildSrc/src/main/kotlin/common/Common.kt
+++ buildSrc/src/main/kotlin/common/Common.kt
@@ -8,16 +8,33 @@ fun platformVersion(project: Project):String{
     return if (project.findProperty("useLatestVersion") == "true"){
         project.logger.lifecycle("building with latestPlatformVersion")
         properties("latestPlatformVersion",project)
-    }else{
+    }else if (project.findProperty("useEAP") == "true"){
+        project.logger.lifecycle("building with EAP")
+        eapVersion(project)
+    }
+    else{
         project.logger.lifecycle("building with platformVersion")
         properties("platformVersion",project)
     }
 }
 
+private fun eapVersion(project: Project): String {
+    return if (project.name == "rider"){
+        properties("riderEAPVersion",project)
+    }else{
+        "LATEST-EAP-SNAPSHOT"
+    }
+}
+
+
+
 fun pythonPluginVersion(project: Project):String{
     return if (project.findProperty("useLatestVersion") == "true"){
         project.logger.lifecycle("building with latestPythonPluginVersion")
         properties("latestPythonPluginVersion",project)
+    }else if (project.findProperty("useEAP") == "true"){
+        project.logger.lifecycle("building with EAP")
+        properties("pythonPluginEAPVersion",project)
     }else{
         project.logger.lifecycle("building with pythonPluginVersion")
         properties("pythonPluginVersion",project)

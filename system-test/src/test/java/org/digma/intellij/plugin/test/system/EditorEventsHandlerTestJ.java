e9f04422b873b9703d39dec9f4ad41ad1a988022
Yoram <yoram@ignissoft.com>
2023-05-24 18:32:23 +0300

    EditorEventsHandlerTestJ runs with services

--- system-test/src/test/java/org/digma/intellij/plugin/test/system/EditorEventsHandlerTestJ.java
+++ system-test/src/test/java/org/digma/intellij/plugin/test/system/EditorEventsHandlerTestJ.java
@@ -5,7 +5,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import com.intellij.testFramework.LightVirtualFile;
-import com.intellij.testFramework.TestDataFile;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import org.digma.intellij.plugin.editor.EditorEventsHandler;
 import org.digma.intellij.plugin.log.Log;
@@ -14,7 +13,6 @@ import org.digma.intellij.plugin.psi.LanguageService;
 import org.digma.intellij.plugin.psi.LanguageServiceLocator;
 import org.junit.Assert;
 import org.junit.Test;
-import java.nio.file.Paths;
 
 public class EditorEventsHandlerTestJ extends LightJavaCodeInsightFixtureTestCase {
 
@@ -24,34 +22,18 @@ public class EditorEventsHandlerTestJ extends LightJavaCodeInsightFixtureTestCas
         return "src/test/resources";
     }
 
-    protected void setPluginXml(@TestDataFile String pluginXml) {
-        final VirtualFile file = myFixture.copyFileToProject(pluginXml, "META-INF/plugin.xml");
-        final PluginBuildConfiguration pluginBuildConfiguration = getPluginBuildConfiguration();
-        pluginBuildConfiguration.setPluginXmlFromVirtualFile(file);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
-
     @Test
     public void testPsiFileNotEmpty() {
-        setPluginXml("plugin.xml");
-        myFixture.setTestDataPath(Paths.get("src/test/resources").toAbsolutePath().toString());
         PsiFile psiFile = myFixture.configureByFile("EditorEventsHandler.java");
-        Assert.assertNotNull("PsiFile should not be null", psiFile);
-        Assert.assertFalse("PsiFile should not be empty", psiFile.getText().isEmpty());
 
-        VirtualFile newFile = new LightVirtualFile("EditorEventsHandler.java");
         Project project = getProject();
         LanguageServiceLocator languageServiceLocator = project.getService(LanguageServiceLocator.class);
+
         LanguageService languageService = languageServiceLocator.locate(psiFile.getLanguage());
-        Log.log(LOGGER::debug, "Found language service {} for :{}",languageService, newFile);
+        Log.log(LOGGER::debug, "Found language service {} for :{}",languageService, psiFile);
 
         DocumentInfo documentInfo = languageService.buildDocumentInfo(psiFile);
-        Log.log(LOGGER::debug, "got DocumentInfo for :{}", newFile);
-
+        Log.log(LOGGER::debug, "got DocumentInfo for :{}", psiFile);
     }
 
 }

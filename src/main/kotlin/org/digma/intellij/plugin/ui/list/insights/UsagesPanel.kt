c3596446f50b3dd5fd79a5e937b0284bce532f16
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-06-04 16:51:05 +0300

    Bugfix/Top Usage display span name even if not linkable (#778)

--- src/main/kotlin/org/digma/intellij/plugin/ui/list/insights/UsagesPanel.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/list/insights/UsagesPanel.kt
@@ -8,13 +8,23 @@ import org.digma.intellij.plugin.editor.getCurrentPageNumberForInsight
 import org.digma.intellij.plugin.editor.updateListOfEntriesToDisplay
 import org.digma.intellij.plugin.model.rest.insights.SpanFlow
 import org.digma.intellij.plugin.model.rest.insights.SpanUsagesInsight
-import org.digma.intellij.plugin.ui.common.*
+import org.digma.intellij.plugin.ui.common.CopyableLabelHtml
+import org.digma.intellij.plugin.ui.common.Html
+import org.digma.intellij.plugin.ui.common.Laf
+import org.digma.intellij.plugin.ui.common.asHtml
+import org.digma.intellij.plugin.ui.common.span
+import org.digma.intellij.plugin.ui.common.spanGrayed
 import org.digma.intellij.plugin.ui.layouts.ResizableFlowLayout
 import org.digma.intellij.plugin.ui.list.openWorkspaceFileForSpan
 import org.digma.intellij.plugin.ui.model.TraceSample
 import org.digma.intellij.plugin.ui.panels.DigmaResettablePanel
-import java.awt.*
-import javax.swing.*
+import java.awt.BorderLayout
+import java.awt.Dimension
+import java.awt.FlowLayout
+import javax.swing.Box
+import javax.swing.BoxLayout
+import javax.swing.JLabel
+import javax.swing.JPanel
 
 
 private const val RECORDS_PER_PAGE = 4
@@ -27,20 +37,20 @@ fun spanUsagesPanel(project: Project, insight: SpanUsagesInsight, moreData: Hash
     val paginationPanel = JPanel()
     val flowsToDisplay = ArrayList<SpanFlow>()
     val flows = insight.flows
-    val spanName = insight.span;
+    val spanName = insight.span
     lastPageNum = countNumberOfPages(flows.size, RECORDS_PER_PAGE)
 
     topUsagePanel = object : DigmaResettablePanel() {
         override fun reset() {
             rebuildTopUsagePanel(
-                    topUsagePanel!!,
-                    flowsToDisplay,
-                    project,
-                    moreData,
-                    spanName
+                topUsagePanel!!,
+                flowsToDisplay,
+                project,
+                moreData,
+                spanName
             )
             rebuildPaginationPanel(paginationPanel, lastPageNum,
-                    flows, topUsagePanel, flowsToDisplay, uniqueInsightId, RECORDS_PER_PAGE, project)
+                flows, topUsagePanel, flowsToDisplay, uniqueInsightId, RECORDS_PER_PAGE, project)
         }
     }
 
@@ -48,35 +58,35 @@ fun spanUsagesPanel(project: Project, insight: SpanUsagesInsight, moreData: Hash
     buildTopUsagePanel(topUsagePanel, flowsToDisplay, project, moreData, spanName)
 
     return createInsightPanel(
-            project = project,
-            insight = insight,
-            title = "Top Usage",
-            description = "",
-            iconsList = null,
-            bodyPanel = topUsagePanel,
-            buttons = null,
-            paginationComponent = buildPaginationRowPanel(lastPageNum, paginationPanel,
-                    flows, topUsagePanel, flowsToDisplay, uniqueInsightId, RECORDS_PER_PAGE, project)
+        project = project,
+        insight = insight,
+        title = "Top Usage",
+        description = "",
+        iconsList = null,
+        bodyPanel = topUsagePanel,
+        buttons = null,
+        paginationComponent = buildPaginationRowPanel(lastPageNum, paginationPanel,
+            flows, topUsagePanel, flowsToDisplay, uniqueInsightId, RECORDS_PER_PAGE, project)
     )
 }
 
 private fun rebuildTopUsagePanel(
-        topUsagePanel: DigmaResettablePanel,
-        flowsToDisplay: List<SpanFlow>,
-        project: Project,
-        moreData: HashMap<String, Any>,
-        spanName: String
+    topUsagePanel: DigmaResettablePanel,
+    flowsToDisplay: List<SpanFlow>,
+    project: Project,
+    moreData: HashMap<String, Any>,
+    spanName: String,
 ) {
     topUsagePanel.removeAll()
     buildTopUsagePanel(topUsagePanel, flowsToDisplay, project, moreData, spanName)
 }
 
 private fun buildTopUsagePanel(
-        topUsagePanel: DigmaResettablePanel,
-        flowsToDisplay: List<SpanFlow>,
-        project: Project,
-        moreData: HashMap<String, Any>,
-        spanName: String
+    topUsagePanel: DigmaResettablePanel,
+    flowsToDisplay: List<SpanFlow>,
+    project: Project,
+    moreData: HashMap<String, Any>,
+    spanName: String,
 ) {
     topUsagePanel.layout = BoxLayout(topUsagePanel, BoxLayout.Y_AXIS)
     topUsagePanel.isOpaque = false
@@ -88,7 +98,7 @@ private fun buildTopUsagePanel(
 }
 
 fun getTopUsagePanel(project: Project, moreData: HashMap<String, Any>,
-                     spanFlow: SpanFlow, spanName: String): JPanel {
+                     spanFlow: SpanFlow, origSpanName: String): JPanel {
 
     val flowPanel = JBPanel<JBPanel<*>>(BorderLayout())
     flowPanel.border = JBUI.Borders.empty()
@@ -100,7 +110,7 @@ fun getTopUsagePanel(project: Project, moreData: HashMap<String, Any>,
 
     val line = JBPanel<JBPanel<*>>(ResizableFlowLayout(FlowLayout.LEFT, 5, 0))
     line.isOpaque = false
-    var spanName = spanName // default, just in case first service is not found
+    var spanName = origSpanName // default, just in case first service is not found
     spanFlow.firstService?.let { firstService ->
         line.add(CopyableLabelHtml(asHtml(spanGrayed(firstService.service + ": "))))
         addSpanLinkIfPossible(project, firstService, moreData, line)
@@ -123,7 +133,7 @@ fun getTopUsagePanel(project: Project, moreData: HashMap<String, Any>,
     flowPanel.add(line, BorderLayout.CENTER)
 
     val buttonToJaeger = buildButtonToJaeger(project, "Trace", spanName, listOf(traceSample))
-    if(buttonToJaeger != null){
+    if (buttonToJaeger != null) {
         val wrapper = JPanel(BorderLayout())
         wrapper.isOpaque = false
         wrapper.border = JBUI.Borders.emptyTop(5)
@@ -135,14 +145,17 @@ fun getTopUsagePanel(project: Project, moreData: HashMap<String, Any>,
 }
 
 fun addSpanLinkIfPossible(project: Project, service: SpanFlow.Service, moreData: HashMap<String, Any>, panel: JPanel) {
-    if (moreData.contains(service.codeObjectId)){
-        val spanName = service.span;
+    val spanName = service.span
+    if (moreData.contains(service.codeObjectId)) {
         val link = ActionLink(spanName) {
             openWorkspaceFileForSpan(project, moreData, service.codeObjectId)
         }
-        var targetClass = service.codeObjectId.substringBeforeLast("\$_\$");
+        val targetClass = service.codeObjectId.substringBeforeLast("\$_\$")
 
         link.toolTipText = asHtml("$targetClass: $spanName")
         panel.add(link)
+    } else {
+        val label = JLabel(asHtml(span(spanName)))
+        panel.add(label)
     }
 }

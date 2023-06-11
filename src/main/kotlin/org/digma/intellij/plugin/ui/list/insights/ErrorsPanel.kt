f4ae4e7750f1cf52bf659dd883ecd935f5986ebf
Shay Keren <shay.keren@gmail.com>
2023-06-11 12:27:46 +0300

    add tab changed events and error click

--- src/main/kotlin/org/digma/intellij/plugin/ui/list/insights/ErrorsPanel.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/list/insights/ErrorsPanel.kt
@@ -31,6 +31,7 @@ fun errorsPanel(project: Project, modelObject: ErrorInsight): JPanel {
         }else "me"
         val errorText = buildLinkTextWithGrayedAndDefaultLabelColorPart(error.errorType ,"From", from)
         val link = ActionLink(errorText) {
+            ActivityMonitor.getInstance(project).registerCustomEvent("error-insight top-error-clicked", null)
             val actionListener: ErrorsActionsService = project.getService(ErrorsActionsService::class.java)
             actionListener.showErrorDetails(error)
         }

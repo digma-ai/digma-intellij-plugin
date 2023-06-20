683d1da8d171a2aad1241d7c574f652e0e29b082
Asaf Chen <achen@digma.ai>
2023-06-20 09:39:56 +0300

    Cleanup bottleneck p50 p99

--- model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/SlowSpanInfo.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/SlowSpanInfo.kt
@@ -7,10 +7,7 @@ import java.beans.ConstructorProperties
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class SlowSpanInfo
 @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-@ConstructorProperties("spanInfo", "p50", "p95", "p99", "probabilityOfBeingBottleneck", "avgDurationWhenBeingBottleneck")
+@ConstructorProperties("spanInfo", "probabilityOfBeingBottleneck", "avgDurationWhenBeingBottleneck")
 constructor(val spanInfo: SpanInfo,
-            val p50: Percentile, // Obsolete
-            val p95: Percentile, // Obsolete
-            val p99: Percentile, // Obsolete
-            val probabilityOfBeingBottleneck: Double?,
-            val avgDurationWhenBeingBottleneck: Duration?)
+            val probabilityOfBeingBottleneck: Double,
+            val avgDurationWhenBeingBottleneck: Duration)

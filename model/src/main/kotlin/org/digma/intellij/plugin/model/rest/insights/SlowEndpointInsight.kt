2da7c5e0a56f967ed944960b3a89e0e5d0f1d69f
Borys Yermakov <byermakov@digma.ai>
2023-06-20 09:15:57 +0200

    Cleanup obsolete props in `SlowEndpointInsight`

--- model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/SlowEndpointInsight.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/SlowEndpointInsight.kt
@@ -25,15 +25,8 @@ data class SlowEndpointInsight
         "serviceName",
         "endpointsMedian",
         "endpointsMedianOfMedians",
-        "endpointsMedianOfP75",
         "endpointsP75",
-        "min",
-        "max",
-        "mean",
         "median",
-        "p75",
-        "p95",
-        "p99",
 )
 constructor(
         override val codeObjectId: String,
@@ -51,15 +44,8 @@ constructor(
         override var serviceName: String,
         val endpointsMedian: Duration,
         val endpointsMedianOfMedians: Duration,
-        val endpointsMedianOfP75: Duration,
         val endpointsP75: Duration,
-        val min: Duration,
-        val max: Duration,
-        val mean: Duration,
         val median: Duration,
-        val p75: Duration,
-        val p95: Duration,
-        val p99: Duration,
 ) : EndpointInsight {
     override val type: InsightType = InsightType.SlowEndpoint
 }

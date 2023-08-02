0b2dfda6d2b2b7ae5457be3f8d27a2cc62df849c
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-08-02 10:04:26 +0300

    Bug fix/add small gap at project panel (#1070)

--- src/main/kotlin/org/digma/intellij/plugin/ui/common/ProjectHomePanel.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/common/ProjectHomePanel.kt
@@ -4,37 +4,37 @@ import com.intellij.openapi.project.Project
 import com.intellij.ui.JBColor
 import com.intellij.ui.dsl.builder.RowLayout
 import com.intellij.ui.dsl.builder.panel
-import com.intellij.ui.dsl.gridLayout.HorizontalAlign
 import java.awt.BorderLayout
 import javax.swing.Icon
 import javax.swing.JPanel
 
-class ProjectHomePanel(val project: Project) : JPanel(){
+
+class ProjectHomePanel(val project: Project) : JPanel() {
 
     init {
         layout = BorderLayout()
         this.background = Laf.Colors.EDITOR_BACKGROUND
         this.isOpaque = true
 
-        add(createPanel(),BorderLayout.CENTER)
+        add(createPanel(), BorderLayout.CENTER)
     }
 
 
-    private fun createPanel():JPanel{
+    private fun createPanel(): JPanel {
         return panel {
             row {
-                icon(getProjectIcon()).horizontalAlign(HorizontalAlign.RIGHT)
-                label(project.name).horizontalAlign(HorizontalAlign.FILL)
-            }.layout(RowLayout.PARENT_GRID)
+                label(asHtml(" "))
+                icon(getProjectIcon())
+                label(project.name)
+            }.layout(RowLayout.LABEL_ALIGNED)
         }.andTransparent()
     }
 
 
-
     private fun getProjectIcon(): Icon {
-        return if (JBColor.isBright()){
+        return if (JBColor.isBright()) {
             Laf.Icons.General.PROJECT_LIGHT
-        }else{
+        } else {
             Laf.Icons.General.PROJECT_DARK
         }
     }

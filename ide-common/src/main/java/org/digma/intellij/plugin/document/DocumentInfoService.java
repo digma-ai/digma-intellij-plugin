a075d38789b99d4fdd704a34b417867c1c2c4f3a
Edan Levy <62389779+EdanLevy@users.noreply.github.com>
2023-08-10 15:04:49 +0300

    implemented async mechanism for insuring that tearDown will not fail the test.

--- ide-common/src/main/java/org/digma/intellij/plugin/document/DocumentInfoService.java
+++ ide-common/src/main/java/org/digma/intellij/plugin/document/DocumentInfoService.java
@@ -117,7 +117,7 @@ public class DocumentInfoService {
     //called after a document is analyzed for code objects
     public void addCodeObjects(@NotNull PsiFile psiFile, @NotNull DocumentInfo documentInfo) {
         Log.log(LOGGER::debug, "Adding DocumentInfo for {},{}", psiFile.getVirtualFile(), documentInfo);
-        Log.test(LOGGER, "Adding DocumentInfo for " + psiFile.getVirtualFile() + "," + documentInfo);
+//        Log.test(LOGGER, "Adding DocumentInfo for " + psiFile.getVirtualFile() + "," + documentInfo);
         DocumentInfoContainer documentInfoContainer = documents.computeIfAbsent(PsiUtils.psiFileToUri(psiFile), file -> new DocumentInfoContainer(psiFile, analyticsService));
         documentInfoContainer.update(documentInfo);
         dominantLanguages.computeIfAbsent(documentInfoContainer.getLanguage().getID(), key -> new MutableInt(0)).increment();
--- system-test/src/test/kotlin/org/digma/intellij/plugin/test/system/MockTestK.kt
+++ system-test/src/test/kotlin/org/digma/intellij/plugin/test/system/MockTestK.kt
@@ -4,7 +4,10 @@ import ai.grazie.nlp.utils.dropWhitespaces
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.LogicalPosition
+import com.intellij.psi.JavaRecursiveElementVisitor
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiMethod
+import com.intellij.refactoring.suggested.startOffset
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 import junit.framework.TestCase
 import kotlinx.coroutines.delay
@@ -12,21 +15,28 @@ import kotlinx.coroutines.runBlocking
 import org.digma.intellij.plugin.analytics.AnalyticsProvider
 import org.digma.intellij.plugin.analytics.AnalyticsService
 import org.digma.intellij.plugin.analytics.RestAnalyticsProvider
+import org.digma.intellij.plugin.document.DocumentInfoContainer
 import org.digma.intellij.plugin.document.DocumentInfoService
 import org.digma.intellij.plugin.idea.psi.java.JavaCodeLensService
 import org.digma.intellij.plugin.log.Log
+import org.digma.intellij.plugin.model.discovery.MethodInfo
 import org.digma.intellij.plugin.model.rest.AboutResult
 import org.digma.intellij.plugin.model.rest.insights.CodeObjectInsight
 import org.digma.intellij.plugin.model.rest.insights.InsightsOfMethodsRequest
+import org.digma.intellij.plugin.model.rest.insights.InsightsOfMethodsResponse
 import org.digma.intellij.plugin.model.rest.version.BackendDeploymentType
+import org.digma.intellij.plugin.test.system.framework.WaitFinishRule
+import org.digma.intellij.plugin.test.system.framework.WaitForAsync
+import org.gradle.internal.impldep.org.junit.Rule
 import org.mockito.Mockito.any
-import org.mockito.Mockito.eq
 import org.mockito.Mockito.mock
-import org.mockito.Mockito.notNull
 import org.mockito.Mockito.`when`
 
 
 class MockTestK : LightJavaCodeInsightFixtureTestCase() {
+
+    @get:Rule
+    val waitRule: WaitFinishRule = WaitFinishRule()
 
     private val logger = Logger.getInstance(MockTestK::class.java)
     private lateinit var analyticsProviderProxyMock: AnalyticsProvider
@@ -36,7 +46,12 @@ class MockTestK : LightJavaCodeInsightFixtureTestCase() {
             return AnalyticsService.getInstance(project)
         }
 
+    // mock test data
     private val environmentList = EnvironmentListMock
+    private val expectedInsightsOfMethodsResponse: InsightsOfMethodsResponse
+        get() {
+            return MockInsightsOfMethodsResponseFactory(environmentList[0])
+        }
 
     override fun setUp() {
         super.setUp()
@@ -51,11 +66,12 @@ class MockTestK : LightJavaCodeInsightFixtureTestCase() {
     }
 
     override fun tearDown() {
+        waitRule.waitForCompletion()
         Log.test(logger, "Tearing down")
-
-        runBlocking {
-            delay(2000L)
-        }
+//
+//        runBlocking {
+//            delay(3000L)
+//        }
 
         super.tearDown()
     }
@@ -76,23 +92,15 @@ class MockTestK : LightJavaCodeInsightFixtureTestCase() {
         mockGetAbout(mock)
         mockGetInsightsOfMethodsForEnv1(mock)
 
-//        `when`(mock.execute<String?>(notNull())).thenAnswer {
-//            val request = it.arguments[0]
-//            Log.test(logger, "In thenAnswer: Requesting $request")
-////            Log.test(logger, "stackTrace: ${Thread.currentThread().stackTrace}")
-//            return@thenAnswer null
-//        }
         return mock
     }
 
+
     private fun mockGetInsightsOfMethodsForEnv1(mock: RestAnalyticsProvider) {
         `when`(mock.getInsightsOfMethods(any(InsightsOfMethodsRequest::class.java)))
-//            .thenReturn(MockInsightsOfMethodsResponseFactory(environmentList[0]))
             .thenAnswer {
-            val request = it.arguments[0] as InsightsOfMethodsRequest
-            Log.test(logger, "In thenAnswer: Requesting InsightsOfMethods for $request")
-            return@thenAnswer MockInsightsOfMethodsResponseFactory(environmentList[0])
-        }
+                return@thenAnswer expectedInsightsOfMethodsResponse
+            }
     }
 
     private fun mockGetAbout(mock: RestAnalyticsProvider) {
@@ -102,18 +110,26 @@ class MockTestK : LightJavaCodeInsightFixtureTestCase() {
     private fun mockGetEnvironments(mock: RestAnalyticsProvider) {
         `when`(mock.getEnvironments()).thenReturn(environmentList)
     }
+    private fun navigateToMethod(editor: Editor, methodLine: Int) {
+        editor.caretModel.moveToLogicalPosition(LogicalPosition(if (methodLine.dec() < 0) 0 else methodLine.dec(), 0))
+    }
 
+    @WaitForAsync
     fun `test that all services are up and running`() {
         Log.test(logger, "Requesting AnalyticsService")
         val analytics = AnalyticsService.getInstance(project)
+        waitRule.signalComplete()
     }
 
+    @WaitForAsync
     fun `test that analytics service is up and running`() {
         Log.test(logger, "Requesting AnalyticsService")
         val analytics = AnalyticsService.getInstance(project)
         TestCase.assertNotNull(analytics)
+        waitRule.signalComplete()
     }
 
+    @WaitForAsync
     fun `test get private field of analytics service`() {
         Log.test(logger, "Requesting AnalyticsService")
         val analytics = analyticsService
@@ -122,14 +138,18 @@ class MockTestK : LightJavaCodeInsightFixtureTestCase() {
         field.isAccessible = true
         val analyticsImpl = field.get(analytics)
         TestCase.assertNotNull(analyticsImpl)
+        waitRule.signalComplete()
     }
 
 
+    @WaitForAsync
     fun `test that analytics service returns mocked environment`() {
         val environments = analyticsService.environments
         TestCase.assertEquals(environmentList, environments)
+        waitRule.signalComplete()
     }
 
+    @WaitForAsync
     fun `test open file and move caret`() {
         val file: PsiFile = myFixture.configureByFile("EditorEventsHandler.java")
         myFixture.openFileInEditor(file.virtualFile)
@@ -160,13 +180,12 @@ class MockTestK : LightJavaCodeInsightFixtureTestCase() {
 
             TestCase.assertNotNull(codeLens)
         }
+        waitRule.signalComplete()
     }
 
-    private fun navigateToMethod(editor: Editor, methodLine: Int) {
-        editor.caretModel.moveToLogicalPosition(LogicalPosition(if (methodLine.dec() < 0) 0 else methodLine.dec(), 0))
-    }
 
-    fun `move caret to method line`() {
+    @WaitForAsync
+    fun `test move caret to method line`() {
         val file = myFixture.configureByFile("EditorEventsHandler.java")
         myFixture.openFileInEditor(file.virtualFile)
         val editor = myFixture.editor
@@ -177,20 +196,28 @@ class MockTestK : LightJavaCodeInsightFixtureTestCase() {
         val caretPosition = editor.caretModel.currentCaret.offset
 
         val endOffset = editor.caretModel.visualLineEnd
+        val startOffset = editor.caretModel.visualLineStart
+        editor.selectionModel.setSelection(startOffset, endOffset)
         val selectedText = editor.selectionModel.selectedText
         Log.test(logger, "Selected text: $selectedText")
 
         TestCase.assertNotNull(selectedText)
         TestCase.assertEquals("public void selectionChanged(@NotNull FileEditorManagerEvent editorManagerEvent) {\n", selectedText?.dropWhitespaces())
+
+        waitRule.signalComplete()
     }
 
+    @WaitForAsync
     fun `test that Digma infoService opened the same file`() {
         val file = myFixture.configureByFile("EditorEventsHandler.java")
         assertTrue(DocumentInfoService.getInstance(project).focusedFile == file.virtualFile)
         myFixture.openFileInEditor(file.virtualFile)
         assertTrue(DocumentInfoService.getInstance(project).focusedFile == file.virtualFile)
+
+        waitRule.signalComplete()
     }
 
+    @WaitForAsync
     fun `test that digma infoService opened the same file after switching to another file`() {
         val file = myFixture.configureByFile("EditorEventsHandler.java")
         myFixture.openFileInEditor(file.virtualFile)
@@ -203,10 +230,14 @@ class MockTestK : LightJavaCodeInsightFixtureTestCase() {
 
         myFixture.openFileInEditor(file.virtualFile)
         assertTrue(documentInfoService.focusedFile == file.virtualFile)
+
+        waitRule.signalComplete()
     }
 
+    @WaitForAsync
     fun `test that Insights are retrieved from analytics service`() {
         val file = myFixture.configureByFile("EditorEventsHandler.java")
+        val expectedMethodInsights = expectedInsightsOfMethodsResponse
         myFixture.openFileInEditor(file.virtualFile)
 
         val documentInfoService = DocumentInfoService.getInstance(project)
@@ -222,14 +253,51 @@ class MockTestK : LightJavaCodeInsightFixtureTestCase() {
         val insights: MutableMap<String, MutableList<CodeObjectInsight>>? = documentInfoContainer?.allMethodWithInsightsMapForCurrentDocument
         TestCase.assertNotNull("Insights are null", insights)
 
-        insights?.forEach {
-            TestCase.assertTrue(it.value.isNotEmpty())
+        insights?.toList()?.forEachIndexed { index, it ->
+            TestCase.assertTrue(it.second.isNotEmpty())
             it.also { (methodId, insights) ->
-                insights.forEach { insight ->
+                insights.forEachIndexed { indexed, insight: CodeObjectInsight ->
                     TestCase.assertEquals(methodId, insight.codeObjectId)
+                    TestCase.assertEquals(expectedMethodInsights.methodsWithInsights[index].insights[indexed], insights[indexed])
                 }
             }
         }
+
+        waitRule.signalComplete()
+    }
+
+
+    @WaitForAsync
+    fun `test get method from file and find the range`() {
+        val file = myFixture.configureByFile("EditorEventsHandler.java")
+        val methodList: MutableList<PsiMethod> = mutableListOf()
+        file.accept(object : JavaRecursiveElementVisitor() {
+            override fun visitMethod(method: PsiMethod) {
+                super.visitMethod(method)
+                methodList.add(method)
+            }
+        })
+
+        val documentInfoService = DocumentInfoService.getInstance(project)
+
+        runBlocking {
+            delay(1000L)
+        }
+
+        val documentInfoContainer: DocumentInfoContainer? = documentInfoService.getDocumentInfo(file.virtualFile)
+
+        val methodRef = documentInfoContainer?.javaClass?.getDeclaredMethod("getMethodInfos")
+        methodRef?.isAccessible = true
+        @Suppress("UNCHECKED_CAST") val methodInfos: List<MethodInfo>? = methodRef?.invoke(documentInfoContainer) as List<MethodInfo>?
+
+        methodInfos?.forEach { method: MethodInfo ->
+            Log.test(logger, "method: ${method.name}, methodOffsetInfile: ${method.offsetAtFileUri},")
+        }
+
+        TestCase.assertNotNull(methodInfos)
+        TestCase.assertEquals(methodList.size, methodInfos?.size)
+
+        waitRule.signalComplete()
     }
 }
 

8b8c8314a28cb143dbee7a4ca22e2fbf73d6b6e9
Edan Levy <62389779+EdanLevy@users.noreply.github.com>
2023-08-10 15:04:49 +0300

    implemented async mechanism for insuring that tearDown will not fail the test.

---
+++ system-test/src/test/kotlin/org/digma/intellij/plugin/test/system/framework/WaitFinishRule.kt
@@ -0,0 +1,40 @@
+package org.digma.intellij.plugin.test.system.framework
+
+import com.intellij.openapi.diagnostic.Logger
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+class WaitFinishRule : TestRule {
+
+        val logger = Logger.getInstance(WaitFinishRule::class.java)
+
+    private var latch: CountDownLatch = CountDownLatch(1)
+    override fun apply(base: Statement, description: Description): Statement {
+        return object : Statement() {
+            override fun evaluate() {
+                if (description.getAnnotation(WaitForAsync::class.java) != null)
+                    latch = CountDownLatch(1)
+                try {
+                    base.evaluate()
+                } finally {
+                   waitForCompletion()
+                }
+            }
+        }
+    }
+
+    fun signalComplete() {
+        latch.countDown()
+    }
+
+    //using this method to wait for async operations to complete, e.g.: in tearDown()
+    fun waitForCompletion() {
+        if (latch.await(10, TimeUnit.SECONDS))
+            logger.info("WaitForAsync completed")
+        else
+            logger.warn("WaitForAsync timeout")
+    }
+}

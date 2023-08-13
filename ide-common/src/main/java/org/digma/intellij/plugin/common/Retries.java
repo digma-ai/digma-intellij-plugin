dfecf7230c490628bbcd598d02c77c3655285329
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2023-08-13 13:06:32 +0300

    Otel agent jars (#1113)
    * minimize otel jars download
    
    * logging
    
    * logging
    
    * logging
    
    * no light service

--- ide-common/src/main/java/org/digma/intellij/plugin/common/Retries.java
+++ ide-common/src/main/java/org/digma/intellij/plugin/common/Retries.java
@@ -3,7 +3,6 @@ package org.digma.intellij.plugin.common;
 import com.intellij.openapi.diagnostic.Logger;
 import org.digma.intellij.plugin.log.Log;
 
-@Deprecated(forRemoval = true)// we have no use for this class
 public class Retries {
 
     private Retries() {
@@ -18,10 +17,11 @@ public class Retries {
     private static void simpleRetry(Runnable runnable,Class<? extends Throwable> retryOn,int backOffMillis,int maxRetries,int retryCount){
 
         try{
+            Log.log(LOGGER::debug,"starting retry "+retryCount);
             runnable.run();
         }catch (Throwable e){
 
-            Log.log(LOGGER::warn,"get exception "+e+ " retry "+retryCount);
+            Log.log(LOGGER::warn,"got exception "+e+ " retry "+retryCount);
             Log.warnWithException(LOGGER,e,"exception in simpleRetry");
 
             if (retryCount == maxRetries){
@@ -32,7 +32,7 @@ public class Retries {
 
             if (retryOn.isAssignableFrom(e.getClass())){
                 try {
-                    Log.log(LOGGER::warn,"sleeping");
+                    Log.log(LOGGER::warn,"sleeping {} millis",backOffMillis);
                     Thread.sleep(backOffMillis);
                 } catch (InterruptedException ex) {/* ignore*/}
                 simpleRetry(runnable,retryOn,backOffMillis,maxRetries,retryCount+1);
--- java/src/main/kotlin/org/digma/intellij/plugin/idea/runcfg/OTELJarProviderStartup.kt
+++ java/src/main/kotlin/org/digma/intellij/plugin/idea/runcfg/OTELJarProviderStartup.kt
@@ -1,11 +1,13 @@
 package org.digma.intellij.plugin.idea.runcfg
 
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 
-class OTELJarProviderStartup: StartupActivity {
+class OTELJarProviderStartup : StartupActivity {
     override fun runActivity(project: Project) {
-        ApplicationManager.getApplication().getService(OTELJarProvider::class.java).ensureDownloaded(project,true)
+        //just call the service so its initializes if it's the first IDE startup.
+        //opening more projects will do nothing because it's an application service.
+        service<OTELJarProvider>()
     }
 }

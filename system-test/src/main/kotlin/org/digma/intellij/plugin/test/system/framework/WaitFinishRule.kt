90366c9b35f0445873f0968ea942b278d4cdcd3e
Edan Levy <62389779+EdanLevy@users.noreply.github.com>
2023-08-15 12:39:47 +0300

    - improved waitFinishRule syntax. - added a messageBusTestListeners that provide functionality to subscribe to the MessageBus and its topics

--- system-test/src/main/kotlin/org/digma/intellij/plugin/test/system/framework/WaitFinishRule.kt
+++ system-test/src/main/kotlin/org/digma/intellij/plugin/test/system/framework/WaitFinishRule.kt
@@ -8,12 +8,18 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import org.digma.intellij.plugin.log.Log
 
+private const val timeoutSeconds = 20L
+
 class WaitFinishRule : TestRule {
 
     private val logger = Logger.getInstance(WaitFinishRule::class.java)
 
     private var latch: CountDownLatch = CountDownLatch(1)
 
+    operator fun invoke() {
+        signalComplete()
+    }
+    
     override fun apply(base: Statement, description: Description): Statement {
         return object : Statement() {
             override fun evaluate() {
@@ -34,7 +40,7 @@ class WaitFinishRule : TestRule {
 
     //using this method to wait for async operations to complete, e.g.: in tearDown()
     fun waitForCompletion() {
-        if (latch.await(100, TimeUnit.SECONDS))
+        if (latch.await(timeoutSeconds, TimeUnit.SECONDS))
             Log.test(logger::info, "WaitForAsync completed")
         else
             Log.test(logger::warn, "WaitForAsync timeout")

53422e39612b6d48cf2c77446bdfdcf08761f09f
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2023-08-17 11:06:57 +0300

    Recent activity 'Troubleshooting' open sidepane

--- src/main/java/org/digma/intellij/plugin/recentactivity/RecentActivityService.java
+++ src/main/java/org/digma/intellij/plugin/recentactivity/RecentActivityService.java
@@ -61,6 +61,7 @@ import org.digma.intellij.plugin.recentactivity.outgoing.LiveDataMessage;
 import org.digma.intellij.plugin.recentactivity.outgoing.LiveDataPayload;
 import org.digma.intellij.plugin.settings.SettingsState;
 import org.digma.intellij.plugin.ui.MainToolWindowCardsController;
+import org.digma.intellij.plugin.ui.ToolWindowShower;
 import org.digma.intellij.plugin.ui.list.insights.JaegerUtilKt;
 import org.digma.intellij.plugin.ui.model.environment.EnvironmentsSupplier;
 import org.digma.intellij.plugin.ui.settings.ApplicationUISettingsChangeNotifier;
@@ -245,7 +246,10 @@ public class RecentActivityService implements Disposable {
                         }
                     }
                     if (JCefMessagesUtils.GLOBAL_OPEN_TROUBLESHOOTING_GUIDE.equalsIgnoreCase(reactMessageRequest.getAction())) {
-                        EDT.ensureEDT(() -> MainToolWindowCardsController.getInstance(project).showTroubleshooting());
+                        EDT.ensureEDT(() -> {
+                            ToolWindowShower.getInstance(project).showToolWindow();
+                            MainToolWindowCardsController.getInstance(project).showTroubleshooting();
+                        });
 
                     }
                 });

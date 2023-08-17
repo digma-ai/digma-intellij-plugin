e0080b5c141ec925b7ffa8b5b4a956060eb9ab53
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-08-17 18:17:05 +0300

    Feature/align insights model for async (#1154)

--- model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/EndpointBreakdownInsight.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/EndpointBreakdownInsight.kt
@@ -2,6 +2,7 @@ package org.digma.intellij.plugin.model.rest.insights
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.digma.intellij.plugin.model.InsightType
 import java.beans.ConstructorProperties
 import java.util.Date
@@ -23,7 +24,6 @@ data class EndpointBreakdownInsight
     "spanInfo",
     "route",
     "serviceName",
-    "components"
 )
 constructor(
     override val codeObjectId: String,
@@ -39,9 +39,24 @@ constructor(
     override val spanInfo: SpanInfo,
     override var route: String,
     override var serviceName: String,
-    val components: List<EndpointBreakdownComponent>,
 ) : EndpointInsight {
     override val type: InsightType = InsightType.EndpointBreakdown
+
+    // hasAsyncSpans. default is false.
+    //  when value false should refer to fraction (percents)
+    //  when value is true should refer to duration
+    @JsonProperty("hasAsyncSpans")
+    val hasAsyncSpans: Boolean = false
+
+    // components - deprecated. instead, use p50Components
+    @JsonProperty("components")
+    val components: List<EndpointBreakdownComponent> = emptyList()
+
+    @JsonProperty("p50Components")
+    val p50Components: List<EndpointBreakdownComponent> = emptyList()
+
+    @JsonProperty("p95Components")
+    val p95Components: List<EndpointBreakdownComponent> = emptyList()
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -51,4 +66,9 @@ data class EndpointBreakdownComponent
 constructor(
     val type: String,
     val fraction: Double,
-)
+) {
+    // duration - temporary nullable. but won't be null when hasAsyncSpans is true
+    @JsonProperty("duration")
+    val duration: Duration? = null
+}
+

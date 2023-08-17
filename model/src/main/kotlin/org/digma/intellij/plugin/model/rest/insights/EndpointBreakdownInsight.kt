e0c43608d669c6a95584279f69f56f66482a3f4c
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-08-17 17:52:02 +0300

    using JsonProperty for new fields

--- model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/EndpointBreakdownInsight.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/EndpointBreakdownInsight.kt
@@ -2,6 +2,7 @@ package org.digma.intellij.plugin.model.rest.insights
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.digma.intellij.plugin.model.InsightType
 import java.beans.ConstructorProperties
 import java.util.Date
@@ -23,10 +24,6 @@ data class EndpointBreakdownInsight
     "spanInfo",
     "route",
     "serviceName",
-    "hasAsyncSpans",
-    "components",
-    "p50Components",
-    "p95Components",
 )
 constructor(
     override val codeObjectId: String,
@@ -42,24 +39,36 @@ constructor(
     override val spanInfo: SpanInfo,
     override var route: String,
     override var serviceName: String,
+) : EndpointInsight {
+    override val type: InsightType = InsightType.EndpointBreakdown
 
     // hasAsyncSpans. default is false.
     //  when value false should refer to fraction (percents)
     //  when value is true should refer to duration
-    val hasAsyncSpans: Boolean,
-    val components: List<EndpointBreakdownComponent>, // deprecated. instead, use p50Components
-    val p50Components: List<EndpointBreakdownComponent>,
-    val p95Components: List<EndpointBreakdownComponent>,
-) : EndpointInsight {
-    override val type: InsightType = InsightType.EndpointBreakdown
+    @JsonProperty("hasAsyncSpans")
+    val hasAsyncSpans: Boolean = false
+
+    // components - deprecated. instead, use p50Components
+    @JsonProperty("components")
+    val components: List<EndpointBreakdownComponent> = emptyList()
+
+    @JsonProperty("p50Components")
+    val p50Components: List<EndpointBreakdownComponent> = emptyList()
+
+    @JsonProperty("p95Components")
+    val p95Components: List<EndpointBreakdownComponent> = emptyList()
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class EndpointBreakdownComponent
 @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-@ConstructorProperties("type", "fraction", "duration")
+@ConstructorProperties("type", "fraction")
 constructor(
     val type: String,
     val fraction: Double,
-    val duration: Duration,
-)
+) {
+    // duration - temporary nullable. but won't be null when hasAsyncSpans is true
+    @JsonProperty("duration")
+    val duration: Duration? = null
+}
+

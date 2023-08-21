c8894d58bb27f993eb0317b0a3ccff7e556561b9
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2023-08-21 21:37:09 +0300

    register startup events earlier in dumb mode

--- ide-common/src/main/java/org/digma/intellij/plugin/common/StartupStartupActivity.java
+++ ide-common/src/main/java/org/digma/intellij/plugin/common/StartupStartupActivity.java
@@ -12,23 +12,6 @@ public class StartupStartupActivity implements StartupActivity.DumbAware {
 
     private static final String RUN_ONCE_ID = "org.digma.runonce.opentoolwindow";
 
-
-
-    /*
-    run setFirstTimePluginLoaded only once and show tool window after first load of the plugin,
-    probably after installation.
-    to test that:
-    the persistence file DigmaPersistence.xml is application persistence and will be under jetbrains config folder,
-    in linux its ./.config/JetBrains/IdeaIC2023.1/options/DigmaPersistence.xml.
-    when running the development instance it will be in the sandbox ander the build folder.
-    delete the file or run ./gradlew clean.
-    the run once file is other.xml, and it will be under jetbrains cache directory, in linux It's somewhere under
-    ~/config/JetBrains. when running the development instance it will be under build/idea-sandbox/config/options/other.xml.
-    delete the file.
-    and test.
-
-
-     */
     @Override
     public void runActivity(@NotNull Project project) {
         RunOnceUtil.runOnceForApp(RUN_ONCE_ID, () -> {

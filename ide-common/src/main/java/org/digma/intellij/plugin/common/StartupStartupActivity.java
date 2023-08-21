686527636da306a29489933245bf0747d707fb91
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2023-08-21 21:35:36 +0300

    register startup events earlier in dumb mode

--- ide-common/src/main/java/org/digma/intellij/plugin/common/StartupStartupActivity.java
+++ ide-common/src/main/java/org/digma/intellij/plugin/common/StartupStartupActivity.java
@@ -1,13 +1,14 @@
 package org.digma.intellij.plugin.common;
 
 import com.intellij.ide.util.RunOnceUtil;
+import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import org.digma.intellij.plugin.posthog.ActivityMonitor;
 import org.digma.intellij.plugin.ui.ToolWindowShower;
 import org.jetbrains.annotations.NotNull;
 
-public class RunOnceStartupActivity implements StartupActivity {
+public class StartupStartupActivity implements StartupActivity.DumbAware {
 
     private static final String RUN_ONCE_ID = "org.digma.runonce.opentoolwindow";
 
@@ -32,7 +33,7 @@ public class RunOnceStartupActivity implements StartupActivity {
     public void runActivity(@NotNull Project project) {
         RunOnceUtil.runOnceForApp(RUN_ONCE_ID, () -> {
             ActivityMonitor.getInstance(project).registerFirstTimePluginLoaded();
-            EDT.ensureEDT(() -> ToolWindowShower.getInstance(project).showToolWindow());
+            project.getService(DumbService.class).runWhenSmart(() -> EDT.ensureEDT(() -> ToolWindowShower.getInstance(project).showToolWindow()));
         });
         ActivityMonitor.getInstance(project).registerPluginLoaded();
     }

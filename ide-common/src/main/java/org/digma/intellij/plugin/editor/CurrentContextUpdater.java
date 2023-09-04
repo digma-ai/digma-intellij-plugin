281620d8a4a0feda02b2ea5af61b0fc35ff5891e
Yoram Shamir <yoram@ignissoft.com>
2023-09-04 17:23:07 +0300

    heavy tests

--- ide-common/src/main/java/org/digma/intellij/plugin/editor/CurrentContextUpdater.java
+++ ide-common/src/main/java/org/digma/intellij/plugin/editor/CurrentContextUpdater.java
@@ -37,7 +37,7 @@ public class CurrentContextUpdater implements Disposable {
    keep the latest method under caret that was fired. it helps us to not call contextChange if the caret is on the same
    method as before.
     */
-    public MethodUnderCaret latestMethodUnderCaret;
+    private MethodUnderCaret latestMethodUnderCaret;
 
     public CurrentContextUpdater(Project project) {
         this.project = project;

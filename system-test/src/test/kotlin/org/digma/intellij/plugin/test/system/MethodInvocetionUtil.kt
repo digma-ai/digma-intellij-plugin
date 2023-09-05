c83a08b76fa8d91bcbaf6b2e2545bd762da04ae2
Edan Levy <62389779+EdanLevy@users.noreply.github.com>
2023-09-05 13:45:42 +0300

    - created private method invocation utils - created spy util functions to prepare for test with default behavior - created func to assert different messages that can be received in execute JavaScript

---
+++ system-test/src/test/kotlin/org/digma/intellij/plugin/test/system/MethodInvocetionUtil.kt
@@ -0,0 +1,15 @@
+package org.digma.intellij.plugin.test.system
+
+import java.lang.reflect.Method
+
+fun invokeMethod(toInvokeOn: Any, methodRef: Method, vararg args: Any): Any? {
+    
+    return methodRef.invoke(toInvokeOn, *args)
+}
+
+fun getMethodReference(obj: Any, methodName: String, vararg argsTypes: Class<*>): Method 
+{
+    val method = obj.javaClass.getDeclaredMethod(methodName, *argsTypes)
+    method.isAccessible = true
+    return method
+}

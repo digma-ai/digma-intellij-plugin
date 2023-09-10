d49b27dfed688480726adccfca2081727f5301db
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2023-09-11 01:05:50 +0300

    minimize read access periods

--- java/src/main/java/org/digma/intellij/plugin/idea/psi/java/JavaSpanNavigationStartupActivity.java
+++ java/src/main/java/org/digma/intellij/plugin/idea/psi/java/JavaSpanNavigationStartupActivity.java
@@ -3,6 +3,7 @@ package org.digma.intellij.plugin.idea.psi.java;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import org.digma.intellij.plugin.common.Backgroundable;
+import org.digma.intellij.plugin.errorreporting.ErrorReporter;
 import org.jetbrains.annotations.NotNull;
 
 public class JavaSpanNavigationStartupActivity implements StartupActivity {
@@ -12,11 +13,15 @@ public class JavaSpanNavigationStartupActivity implements StartupActivity {
     public void runActivity(@NotNull Project project) {
 
         Backgroundable.executeOnPooledThread(() -> {
-            var javaSpanNavigationProvider = JavaSpanNavigationProvider.getInstance(project);
-            javaSpanNavigationProvider.buildSpanNavigation();
+            try {
+                var javaSpanNavigationProvider = JavaSpanNavigationProvider.getInstance(project);
+                javaSpanNavigationProvider.buildSpanNavigation();
 
-            var javaEndpointNavigationProvider = JavaEndpointNavigationProvider.getInstance(project);
-            javaEndpointNavigationProvider.buildEndpointNavigation();
+                var javaEndpointNavigationProvider = JavaEndpointNavigationProvider.getInstance(project);
+                javaEndpointNavigationProvider.buildEndpointNavigation();
+            } catch (Exception e) {
+                ErrorReporter.getInstance().reportError("JavaSpanNavigationStartupActivity.runActivity", e);
+            }
         });
     }
 
--- python/src/main/java/org/digma/intellij/plugin/psi/python/PythonSpanNavigationStartupActivity.java
+++ python/src/main/java/org/digma/intellij/plugin/psi/python/PythonSpanNavigationStartupActivity.java
@@ -3,6 +3,7 @@ package org.digma.intellij.plugin.psi.python;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import org.digma.intellij.plugin.common.Backgroundable;
+import org.digma.intellij.plugin.errorreporting.ErrorReporter;
 import org.jetbrains.annotations.NotNull;
 
 public class PythonSpanNavigationStartupActivity implements StartupActivity {
@@ -12,8 +13,12 @@ public class PythonSpanNavigationStartupActivity implements StartupActivity {
     public void runActivity(@NotNull Project project) {
 
         Backgroundable.executeOnPooledThread(() -> {
-            PythonSpanNavigationProvider pythonSpanNavigationProvider = PythonSpanNavigationProvider.getInstance(project);
-            pythonSpanNavigationProvider.buildSpanNavigation();
+            try {
+                PythonSpanNavigationProvider pythonSpanNavigationProvider = PythonSpanNavigationProvider.getInstance(project);
+                pythonSpanNavigationProvider.buildSpanNavigation();
+            } catch (Exception e) {
+                ErrorReporter.getInstance().reportError("PythonSpanNavigationStartupActivity.runActivity", e);
+            }
         });
 
     }

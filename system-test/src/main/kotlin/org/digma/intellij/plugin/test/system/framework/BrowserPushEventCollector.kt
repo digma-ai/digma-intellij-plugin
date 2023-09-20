2c86443f5680013390f3f44529074786badff604
Edan Levy <62389779+EdanLevy@users.noreply.github.com>
2023-09-20 15:37:56 +0300

    - implemented event queue to assert in the test case. - fixed bullet five data and the resource files to not have the same mothod name in both documents

---
+++ system-test/src/main/kotlin/org/digma/intellij/plugin/test/system/framework/BrowserPushEventCollector.kt
@@ -0,0 +1,50 @@
+package org.digma.intellij.plugin.test.system.framework
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.JsonNodeFactory
+import com.intellij.openapi.diagnostic.Logger
+import org.digma.intellij.plugin.log.Log
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentLinkedQueue
+
+
+class BrowserPushEventCollector {
+
+    private val logger = Logger.getInstance(BrowserPushEventCollector::class.java)
+
+    private val actionsQueueMap: ConcurrentHashMap<String, ConcurrentLinkedQueue<JsonNode>> = ConcurrentHashMap() // <action, queue<JsonNode>> 
+
+
+    /*
+    * adds an event to the queue for the given action. creates a new queue if action is not present.
+    * @param action - action name
+    * @param browserJson - json string to be sent to browser
+    * 
+     */
+    fun addEvent(action: String, browserJson: JsonNode) {
+        Log.test(logger::info, "adding event for action $action, size of the queue: {}", actionsQueueMap[action]?.size ?: 0)
+        actionsQueueMap.computeIfAbsent(action) { ConcurrentLinkedQueue() }.add(browserJson)
+    }
+
+    /*
+    * @return empty string if no event for action found
+     */
+    fun popEvent(action: String): JsonNode {
+        return actionsQueueMap[action]?.poll() ?: JsonNodeFactory.instance.nullNode()
+    }
+
+    /*
+    * @return true if there are events for the given action
+     */
+    fun hasEvents(action: String): Boolean {
+        return actionsQueueMap[action]?.isNotEmpty() ?: false
+    }
+
+    /*
+    * clears all events for the given action
+     */
+    fun clearEvents(action: String) {
+        actionsQueueMap[action]?.clear()
+    }
+
+}
--- system-test/src/test/kotlin/org/digma/intellij/plugin/test/system/DigmaTestCase.kt
+++ system-test/src/test/kotlin/org/digma/intellij/plugin/test/system/DigmaTestCase.kt
@@ -1,19 +1,28 @@
 package org.digma.intellij.plugin.test.system
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.psi.PsiFile
 import com.intellij.testFramework.PlatformTestUtil
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
+import org.cef.browser.CefBrowser
 import org.digma.intellij.plugin.analytics.AnalyticsService
 import org.digma.intellij.plugin.analytics.RestAnalyticsProvider
 import org.digma.intellij.plugin.document.DocumentInfoService
+import org.digma.intellij.plugin.jcef.common.JCefBrowserUtil
 import org.digma.intellij.plugin.log.Log
+import org.digma.intellij.plugin.test.system.framework.BrowserPushEventCollector
 import org.digma.intellij.plugin.test.system.framework.MessageBusTestListeners
 import org.digma.intellij.plugin.test.system.framework.mockRestAnalyticsProvider
 import org.digma.intellij.plugin.ui.recentactivity.RecentActivityService
+import org.digma.intellij.plugin.ui.recentactivity.RecentActivityUpdater
+import org.mockito.Mockito
 
 
 open class DigmaTestCase : LightJavaCodeInsightFixtureTestCase() {
@@ -24,11 +33,15 @@ open class DigmaTestCase : LightJavaCodeInsightFixtureTestCase() {
 
     protected lateinit var messageBusTestListeners: MessageBusTestListeners
 
+    protected lateinit var jsonCollector: BrowserPushEventCollector
+
+    val objectMapper: ObjectMapper = JCefBrowserUtil.getObjectMapper()
+
     protected val analyticsService: AnalyticsService
         get() {
             return AnalyticsService.getInstance(project)
         }
-    
+
     protected val recentActivityService: RecentActivityService
         get() {
             return project.service<RecentActivityService>()
@@ -39,6 +52,10 @@ open class DigmaTestCase : LightJavaCodeInsightFixtureTestCase() {
     @Volatile
     protected var readyToAssert: Boolean = false
         private set
+    protected val recentActivityUpdater: RecentActivityUpdater
+        get() {
+            return project.getService(RecentActivityUpdater::class.java)
+        }
 
 
     protected fun readyToTest() {
@@ -54,6 +71,7 @@ open class DigmaTestCase : LightJavaCodeInsightFixtureTestCase() {
         super.setUp()
         mockAnalyticsProvider = mockRestAnalyticsProvider(project)
         messageBusTestListeners = MessageBusTestListeners(project.messageBus)
+        jsonCollector = BrowserPushEventCollector()
         readyToAssert = false
     }
 
@@ -72,12 +90,35 @@ open class DigmaTestCase : LightJavaCodeInsightFixtureTestCase() {
         return systemTestDataPath
     }
 
+    fun setupExecuteJSOf(spiedCefBrowser: CefBrowser) {
+        Mockito.doAnswer {
+            it.getArgument(0, String::class.java)
+                .also { payload ->
+                    val stripedPayload = payload.substringAfter("window.postMessage(").substringBeforeLast(");")
+                    val jsonpayload = objectMapper.readTree(stripedPayload)
+                    val action = jsonpayload.get("action").asText()
+                    val browserJson: JsonNode = jsonpayload.get("payload")
+                    jsonCollector.addEvent(action, browserJson)
+                }
+        }.`when`(spiedCefBrowser).executeJavaScript(Mockito.anyString(), Mockito.anyString(), Mockito.anyInt())
+    }
+
+
     fun waitForDocumentInfoToLoad(file: PsiFile) {
         runBlocking {
             while (DocumentInfoService.getInstance(project).getDocumentInfo(file.virtualFile) == null) {
                 delay(100L)
             }
         }
+    }
+
+    fun waitForRecentActivityToLoad() {
+        runBlocking {
+            while (recentActivityService.getRecentActivities(analyticsService.environment.getEnvironments()) == null) {
+                delay(100L)
+            }
+        }
+        PlatformTestUtil.dispatchAllEventsInIdeEventQueue()
     }
 
     fun waitFor(timeMillis: Long, reason: String) {
@@ -87,4 +128,31 @@ open class DigmaTestCase : LightJavaCodeInsightFixtureTestCase() {
         }
     }
 
+    fun waitForAndDispatch(timeMillis: Long, reason: String) {
+        waitFor(timeMillis, reason)
+        PlatformTestUtil.dispatchAllEventsInIdeEventQueue()
+    }
+
+
+    fun assertQueueOfActionWithinTimeout(action: String, timeout: Long, assertionFunction: (JsonNode) -> Unit) {
+        runBlocking {
+            withTimeoutOrNull(timeout) {
+                while (isActive) {
+                    if (jsonCollector.hasEvents(action)) {
+                        val event = jsonCollector.popEvent(action)
+                        Log.test(logger::info, "assertQueueOfActionWithinTimeout found event for action $action")
+                        try {
+                            assertionFunction(event)
+                            break
+                        } catch (e: Throwable) {
+                            Log.test(logger::info, "assertQueueOfActionWithinTimeout failed for action $action\n", e)
+                            delay(500L)
+                        }
+                    }
+                }
+            } ?: fail("timeout waiting for $action to pass assertion")
+
+
+        }
+    }
 }

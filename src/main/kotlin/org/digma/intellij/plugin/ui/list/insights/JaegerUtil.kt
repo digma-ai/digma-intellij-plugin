13f57ca2f093990207ff692dec3a25ffe172ac9f
Roni Dover <roni.dover@gmail.com>
2023-09-25 00:37:56 -0700

    updated jaeger token replace

--- src/main/kotlin/org/digma/intellij/plugin/ui/list/insights/JaegerUtil.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/list/insights/JaegerUtil.kt
@@ -99,8 +99,15 @@ fun openJaegerFromRecentActivity(
     val settingsState = SettingsState.getInstance()
 
     val jaegerUrlEmbedPart = "&uiEmbed=v0"
+    val jaegerUrl: String
     val jaegerBaseUrl = settingsState.jaegerUrl?.trim()?.trimEnd('/')
-    val jaegerUrl = "${jaegerBaseUrl}/trace/${traceId.lowercase()}?cohort=${traceId.lowercase()}${jaegerUrlEmbedPart}"
+    if (settingsState.jaegerLinkMode==LinkMode.External && jaegerBaseUrl!=null && jaegerBaseUrl.contains("\${TRACE_ID}")){
+        jaegerUrl=jaegerBaseUrl.replace("\${TRACE_ID}",traceId.lowercase())
+    }
+    else{
+        jaegerUrl = "${jaegerBaseUrl}/trace/${traceId.lowercase()}?cohort=${traceId.lowercase()}${jaegerUrlEmbedPart}"
+
+    }
 
     when(settingsState.jaegerLinkMode){
 
@@ -143,7 +150,7 @@ fun openJaegerFromInsight(
     val embedPart = "&uiEmbed=v0"
 
     val trace1 = traceId.lowercase();
-    if (jaegerBaseUrl!=null && jaegerBaseUrl.contains("\${TRACE_ID}")){
+    if (settingsState.jaegerLinkMode==LinkMode.External && jaegerBaseUrl!=null && jaegerBaseUrl.contains("\${TRACE_ID}")){
         jaegerUrl=jaegerBaseUrl.replace("\${TRACE_ID}",trace1)
     }
     else{

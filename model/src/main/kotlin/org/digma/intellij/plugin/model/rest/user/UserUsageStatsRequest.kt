1bc204d352def05505dea5c2c88415fcfeeb064f
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-11-06 17:50:09 +0200

    accountId is optional

--- model/src/main/kotlin/org/digma/intellij/plugin/model/rest/user/UserUsageStatsRequest.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/user/UserUsageStatsRequest.kt
@@ -1,16 +1,12 @@
 package org.digma.intellij.plugin.model.rest.user
 
 import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import java.beans.ConstructorProperties
 
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class UserUsageStatsRequest
 @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-@ConstructorProperties("accountId")
 constructor(
-    val accountId: String,
+    val accountId: String?,
 ) {
-
+    constructor() : this(null)
 }

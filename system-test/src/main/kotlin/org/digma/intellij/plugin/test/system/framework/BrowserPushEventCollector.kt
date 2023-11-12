4a45e0d57245e64d455fdc99324e1b4b5872bb03
Edan Levy <62389779+EdanLevy@users.noreply.github.com>
2023-11-12 13:12:11 +0200

    - Cleanup tests  - renamed Variables  - combined systemTest and InfraTest.

--- system-test/src/main/kotlin/org/digma/intellij/plugin/test/system/framework/BrowserPushEventCollector.kt
+++ system-test/src/main/kotlin/org/digma/intellij/plugin/test/system/framework/BrowserPushEventCollector.kt
@@ -15,33 +15,34 @@ class BrowserPushEventCollector {
     private val actionsQueueMap: ConcurrentHashMap<String, ConcurrentLinkedQueue<JsonNode>> = ConcurrentHashMap() // <action, queue<JsonNode>> 
 
 
-    /*
-    * adds an event to the queue for the given action. creates a new queue if action is not present.
-    * @param action - action name
-    * @param browserJson - json string to be sent to browser
-    * 
+    /**
+     * adds an event to the queue for the given action. creates a new queue if action is not present.
+     * @param action - action name
+     * @param browserJson - json string to be sent to browser
+     *
      */
     fun addEvent(action: String, browserJson: JsonNode) {
         Log.test(logger::info, "adding event for action $action, size of the queue: {}", actionsQueueMap[action]?.size ?: 0)
         actionsQueueMap.computeIfAbsent(action) { ConcurrentLinkedQueue() }.add(browserJson)
     }
 
-    /*
-    * @return empty string if no event for action found
+    /**
+     * @return empty string if no event for action found
      */
     fun popEvent(action: String): JsonNode {
         return actionsQueueMap[action]?.poll() ?: JsonNodeFactory.instance.nullNode()
     }
 
-    /*
-    * @return true if there are events for the given action
+    /**
+     * @return true if there are events for the given action
      */
     fun hasEvents(action: String): Boolean {
         return actionsQueueMap[action]?.isNotEmpty() ?: false
     }
 
-    /*
-    * clears all events for the given action
+    /**
+     * clears all events for the given action
+     * @param action - action name
      */
     fun clearEvents(action: String) {
         actionsQueueMap[action]?.clear()

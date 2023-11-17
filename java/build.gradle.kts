9af4b9e052df25d790c7a89b2bc115ee399c992c
oleksandrh <alex.hrebeniuk@gmail.com>
2023-11-17 16:18:25 +0200

    upgraded java agent version

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -32,7 +32,7 @@ tasks{
         src(
             listOf(
 //              "https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar",
-                "https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v1.29.0/opentelemetry-javaagent.jar",
+                "https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v1.31.0/opentelemetry-javaagent.jar",
                 "https://github.com/digma-ai/otel-java-instrumentation/releases/latest/download/digma-otel-agent-extension.jar")
         )
 

957f2b13867d59fc6c72d9f14b1dc576eb66bd1d
oleksandrh <alex.hrebeniuk@gmail.com>
2023-11-29 12:31:23 +0200

    changed setEnableOpenDevToolsMenuItem

--- ide-common/src/main/java/org/digma/intellij/plugin/common/JBCefBrowserBuilderCreator.java
+++ ide-common/src/main/java/org/digma/intellij/plugin/common/JBCefBrowserBuilderCreator.java
@@ -10,7 +10,7 @@ public class JBCefBrowserBuilderCreator {
         if (SystemInfo.isLinux) {
             jbCefBrowserBuilder.setOffScreenRendering(true); // setting it to false may cause focus issues on some linux os
         }
-        jbCefBrowserBuilder.setEnableOpenDevToolsMenuItem(true);
+        jbCefBrowserBuilder.setEnableOpenDevToolsMenuItem(false);
         return jbCefBrowserBuilder;
     }
 }

ffc78ac32518ddac53b5999c83b1f0cffff99ef5
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-12-05 16:47:05 +0200

    skip accountId since have encode/decode issues

--- model/src/main/kotlin/org/digma/intellij/plugin/model/rest/user/UserUsageStatsRequest.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/user/UserUsageStatsRequest.kt
@@ -6,7 +6,8 @@ import com.fasterxml.jackson.annotation.JsonCreator
 data class UserUsageStatsRequest
 @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
 constructor(
-    val accountId: String?,
+//  val accountId: String?,
+    val dummy: String?,
 ) {
     constructor() : this(null)
 }

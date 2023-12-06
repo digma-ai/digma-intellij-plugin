a73ec86e5a141961d11e5834a009dd8bcacbaac7
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-12-06 15:49:35 +0200

    adjust otel.javaagent.experimental.field-injection.enabled when test and has mockito

--- java/src/main/kotlin/org/digma/intellij/plugin/idea/runcfg/AutoOtelAgentRunConfigurationWrapper.kt
+++ java/src/main/kotlin/org/digma/intellij/plugin/idea/runcfg/AutoOtelAgentRunConfigurationWrapper.kt
@@ -172,6 +172,14 @@ class AutoOtelAgentRunConfigurationWrapper : RunConfigurationWrapper {
             retVal = retVal
                 .plus("-Dotel.resource.attributes=\"$envPart\"")
                 .plus(" ")
+
+            var hasMockito = true //TODO: really check for mockito in the module
+            if (hasMockito) { // mockito
+                // based on git issue https://github.com/open-telemetry/opentelemetry-java-instrumentation/issues/8862#issuecomment-1619722050 it seems to help
+                retVal = retVal
+                    .plus("-Dotel.javaagent.experimental.field-injection.enabled=false")
+                    .plus(" ")
+            }
         }
 
         retVal = retVal

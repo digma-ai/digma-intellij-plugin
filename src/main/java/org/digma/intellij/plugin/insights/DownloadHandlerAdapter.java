def6128a40d291e821d2ace05813a48d0506da85
Shay Keren <shay.keren@gmail.com>
2023-12-14 20:31:24 +0200

    fix DownloadHandlerAdapter

--- src/main/java/org/digma/intellij/plugin/insights/DownloadHandlerAdapter.java
+++ src/main/java/org/digma/intellij/plugin/insights/DownloadHandlerAdapter.java
@@ -1,7 +1,19 @@
 package org.digma.intellij.plugin.insights;
 
-import org.cef.handler.CefDownloadHandlerAdapter;
+import org.cef.browser.CefBrowser;
+import org.cef.callback.CefBeforeDownloadCallback;
+import org.cef.callback.CefDownloadItem;
+import org.cef.callback.CefDownloadItemCallback;
+import org.cef.handler.CefDownloadHandler;
 
-class DownloadHandlerAdapter extends CefDownloadHandlerAdapter {
+class DownloadHandlerAdapter implements CefDownloadHandler {
+    @Override
+    public void onBeforeDownload(CefBrowser browser, CefDownloadItem downloadItem, String suggestedName, CefBeforeDownloadCallback callback) {
+        callback.Continue(suggestedName, true);
+    }
 
+    @Override
+    public void onDownloadUpdated(CefBrowser browser, CefDownloadItem downloadItem, CefDownloadItemCallback callback) {
+
+    }
 }

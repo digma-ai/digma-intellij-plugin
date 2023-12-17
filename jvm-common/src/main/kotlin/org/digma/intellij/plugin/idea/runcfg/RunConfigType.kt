5735ef5d163ffba14f8b77465216ee63e292baeb
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2023-12-17 17:38:34 +0200

    support kotlin run config (#1494)

--- jvm-common/src/main/kotlin/org/digma/intellij/plugin/idea/runcfg/RunConfigType.kt
+++ jvm-common/src/main/kotlin/org/digma/intellij/plugin/idea/runcfg/RunConfigType.kt
@@ -2,6 +2,7 @@ package org.digma.intellij.plugin.idea.runcfg
 
 enum class RunConfigType(val isTest: Boolean) {
     JavaRun(false),
+    KotlinRun(false),
     GradleRun(false),
     MavenRun(false),
     JavaTest(true),

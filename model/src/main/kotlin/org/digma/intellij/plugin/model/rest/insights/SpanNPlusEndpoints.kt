05b8d9665d4a35873b04675301ccebe639b70421
Shay Keren <shay.keren@gmail.com>
2023-12-20 11:08:23 +0200

    add traceid and duration to spanNplusEndpoints

--- model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/SpanNPlusEndpoints.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/SpanNPlusEndpoints.kt
@@ -7,11 +7,13 @@ import java.beans.ConstructorProperties
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class SpanNPlusEndpoints
 @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-@ConstructorProperties("endpointInfo", "occurrences", "severity", "impact", "criticality")
+@ConstructorProperties("endpointInfo", "occurrences", "severity", "impact", "criticality", "traceId", "duration")
 constructor(
         val endpointInfo: EndpointInfo,
         val occurrences: Number,
         val severity: Double,
         val impact: Double,
-        val criticality: Double
+        val criticality: Double,
+        val traceId: String,
+        val duration: Duration
 )

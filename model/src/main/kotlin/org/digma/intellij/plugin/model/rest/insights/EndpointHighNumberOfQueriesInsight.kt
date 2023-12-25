01b4421b9b392b743331a9ea13296404256b46ca
Shay Keren <shay.keren@gmail.com>
2023-12-25 13:21:21 +0200

    high-number-of-queries-insight (#1515)
    * high-number-of-queries-insight
    
    * define the EndpointHighNumberOfQueriesInsight
    
    * add missing fields
    
    ---------
    
    Co-authored-by: Arik Sher <104715391+arik-dig@users.noreply.github.com>

---
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/EndpointHighNumberOfQueriesInsight.kt
@@ -0,0 +1,67 @@
+package org.digma.intellij.plugin.model.rest.insights
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.digma.intellij.plugin.model.InsightType
+import java.beans.ConstructorProperties
+import java.util.Date
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class EndpointHighNumberOfQueriesInsight
+@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+@ConstructorProperties(
+    "codeObjectId",
+    "environment",
+    "scope",
+    "importance",
+    "decorators",
+    "actualStartTime",
+    "customStartTime",
+    "prefixedCodeObjectId",
+    "isRecalculateEnabled",
+    "shortDisplayInfo",
+    "spanInfo",
+    "route",
+    "serviceName",
+    "severity",
+    "impact",
+    "criticality"
+)
+constructor(
+    override val codeObjectId: String,
+    override val environment: String,
+    override val scope: String,
+    override val importance: Int,
+    override val decorators: List<CodeObjectDecorator>?,
+    override val actualStartTime: Date?,
+    override val customStartTime: Date?,
+    override val prefixedCodeObjectId: String?,
+    @get:JsonProperty("isRecalculateEnabled")
+    @param:JsonProperty("isRecalculateEnabled")
+    override val isRecalculateEnabled: Boolean,
+    override val shortDisplayInfo: ShortDisplayInfo?,
+    override val spanInfo: SpanInfo,
+    override var route: String,
+    override var serviceName: String,
+    override val severity: Double,
+    override val impact: Double,
+    override val criticality: Double,
+) : EndpointInsight {
+    override val type: InsightType = InsightType.EndpointHighNumberOfQueries
+
+    @JsonProperty("queriesCount")
+    val queriesCount: Int = 0
+
+    @JsonProperty("typicalCount")
+    val typicalCount: Int = 0
+
+    @JsonProperty("traceId")
+    val traceId: String? = null
+
+    @JsonProperty("medianDuration")
+    val medianDuration: Duration? = null
+
+    @JsonProperty("requestFraction")
+    val requestFraction: Double? = null
+}

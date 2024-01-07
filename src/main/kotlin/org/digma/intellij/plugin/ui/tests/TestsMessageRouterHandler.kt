d612918713fd442f01dfbbb627f5c5414fe32bfb
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2024-01-07 16:40:31 +0200

    start with TestsMessageRouterHandler SPAN/GET_LATEST_DATA and SPAN/SET_LATEST_DATA

--- src/main/kotlin/org/digma/intellij/plugin/ui/tests/TestsMessageRouterHandler.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/tests/TestsMessageRouterHandler.kt
@@ -4,42 +4,83 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import org.cef.browser.CefBrowser
-import org.digma.intellij.plugin.jcef.common.JCefMessagesUtils
+import org.digma.intellij.plugin.analytics.AnalyticsServiceException
+import org.digma.intellij.plugin.common.Backgroundable
+import org.digma.intellij.plugin.errorreporting.ErrorReporter
 import org.digma.intellij.plugin.log.Log
-import org.digma.intellij.plugin.ui.common.ObservabilityUtil
 import org.digma.intellij.plugin.ui.jcef.BaseMessageRouterHandler
-import org.digma.intellij.plugin.ui.jcef.RegistrationEventHandler
+import org.digma.intellij.plugin.ui.jcef.executeWindowPostMessageJavaScript
+import org.digma.intellij.plugin.ui.jcef.model.ErrorPayload
+import org.digma.intellij.plugin.ui.jcef.model.Payload
 import org.digma.intellij.plugin.ui.jcef.updateDigmaEngineStatus
-import org.digma.intellij.plugin.ui.recentactivity.RecentActivityUpdater
+import org.digma.intellij.plugin.ui.tests.model.SetLatestTestsMessage
+import java.util.Collections
 
 class TestsMessageRouterHandler(project: Project) : BaseMessageRouterHandler(project) {
+
     override fun getOriginForTroubleshootingEvent(): String {
         return "tests"
     }
 
     override fun doOnQuery(project: Project, browser: CefBrowser, requestJsonNode: JsonNode, rawRequest: String, action: String) {
+
+        Log.log(logger::trace, project, "got action '$action' with message $requestJsonNode")
+
         when (action) {
+            "TESTS/INITIALIZE" -> initialize(project, browser)
+            "TESTS/SPAN/GET_LATEST_DATA" -> spanGetLatestData(project, browser, requestJsonNode, rawRequest)
 
-            "TESTS/INITIALIZE" -> {
-                updateDigmaEngineStatus(project, browser)
-                //...
+            else -> {
+                Log.log(logger::warn, "got unexpected action='$action'")
             }
-
-
-            JCefMessagesUtils.GLOBAL_REGISTER -> {
-                RegistrationEventHandler.getInstance(project).register(requestJsonNode)
-            }
-
-            JCefMessagesUtils.GLOBAL_SET_OBSERVABILITY -> {
-                val isEnabledObservability =
-                    objectMapper.readTree(requestJsonNode.get("payload").toString()).get("isObservabilityEnabled").asBoolean()
-                Log.log(logger::trace, "updateSetObservability(Boolean) called")
-                ObservabilityUtil.updateObservabilityValue(project, isEnabledObservability)
-
-                project.service<RecentActivityUpdater>().updateSetObservability(isEnabledObservability)
-            }
-
         }
+    }
+
+    fun initialize(project: Project, browser: CefBrowser) {
+        updateDigmaEngineStatus(project, browser)
+    }
+
+    fun spanGetLatestData(project: Project, browser: CefBrowser, requestJsonNode: JsonNode, rawRequest: String) {
+        Backgroundable.executeOnPooledThread {
+            try {
+                val payloadNode: JsonNode = objectMapper.readTree(requestJsonNode.get("payload").toString())
+                val spanCodeObjectId: String = payloadNode.get("spanCodeObjectId").textValue()
+                val pageNumber: Int = payloadNode.get("pageNumber").intValue()
+                val pageSize: Int = payloadNode.get("pageSize").intValue()
+
+                var environments: Set<String> = Collections.emptySet()
+                val envsNode: JsonNode = payloadNode.get("environments")
+                if (envsNode.isArray() && envsNode.elements().hasNext()) {
+                    val envsArray = objectMapper.convertValue(envsNode, Array<String>::class.java)
+                    environments = envsArray.toSet()
+                }
+
+                val testsOfSpanJson = project.service<TestsService>().getLatestTestsOfSpan(spanCodeObjectId, environments, pageNumber, pageSize)
+
+                Log.log(logger::trace, project, "got tests of span {}", testsOfSpanJson)
+                val payload = objectMapper.readTree(testsOfSpanJson)
+                val message = SetLatestTestsMessage("digma", "TESTS/SPAN/SET_LATEST_DATA", Payload(payload))
+                Log.log(logger::trace, project, "sending TESTS/SPAN/SET_LATEST_DATA message")
+
+                executeWindowPostMessageJavaScript(browser, objectMapper.writeValueAsString(message))
+
+            } catch (e: AnalyticsServiceException) {
+                Log.warnWithException(logger, e, "error setting tests of span data")
+                val message = SetLatestTestsMessage("digma", "TESTS/SPAN/SET_LATEST_DATA", Payload(null, ErrorPayload(e.getMeaningfulMessage())))
+                Log.log(logger::trace, project, "sending TESTS/SPAN/SET_LATEST_DATA message with error")
+                executeWindowPostMessageJavaScript(browser, objectMapper.writeValueAsString(message))
+                ErrorReporter.getInstance().reportError(project, "TestsMessageRouterHandler.SPAN/SET_LATEST_DATA", e)
+            } catch (e: Exception) {
+                Log.warnWithException(logger, e, "error setting tests of span data")
+                val message = SetLatestTestsMessage("digma", "TESTS/SPAN/SET_LATEST_DATA", Payload(null, ErrorPayload(e.toString())))
+                Log.log(logger::trace, project, "sending TESTS/SPAN/SET_LATEST_DATA message with error")
+                executeWindowPostMessageJavaScript(browser, objectMapper.writeValueAsString(message))
+                ErrorReporter.getInstance().reportError(project, "TestsMessageRouterHandler.SPAN/SET_LATEST_DATA", e)
+                //let BaseMessageRouterHandler handle the exception too in case it does something meaningful, worst case it will just log
+                // the error again
+                throw e
+            }
+        } // Backgroundable
     }
 
 }
--- src/main/kotlin/org/digma/intellij/plugin/ui/tests/TestsService.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/tests/TestsService.kt
@@ -2,12 +2,34 @@ package org.digma.intellij.plugin.ui.tests
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
+import org.digma.intellij.plugin.analytics.AnalyticsService
+import org.digma.intellij.plugin.analytics.AnalyticsServiceException
+import org.digma.intellij.plugin.errorreporting.ErrorReporter
+import org.digma.intellij.plugin.log.Log
 
 @Service(Service.Level.PROJECT)
 class TestsService(val project: Project) : Disposable {
+
+    private val logger = Logger.getInstance(this::class.java)
+
     override fun dispose() {
         //nothing to do
+    }
+
+    // return JSON as string (type LatestTestsOfSpanResponse)
+    fun getLatestTestsOfSpan(spanCodeObjectId: String, environments: Set<String>, pageNumber: Int, pageSize: Int): String {
+        try {
+            val json = project.service<AnalyticsService>()
+                .getLatestTestsOfSpan(spanCodeObjectId, environments, pageNumber, pageSize)
+            return json
+        } catch (e: AnalyticsServiceException) {
+            Log.warnWithException(logger, project, e, "exception in getLatestTestsOfSpan")
+            ErrorReporter.getInstance().reportError(project, "TestsService.getLatestTestsOfSpan", e)
+            throw e
+        }
     }
 
 }

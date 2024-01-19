417fafc0af297e11d14e8848713de7355ddbc3f4
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-01-20 01:43:05 +0200

    catch-exception-in-RunConfigurationStartupActivity (#1615)

--- jvm-common/src/main/kotlin/org/digma/intellij/plugin/idea/runcfg/RunConfigurationStartupActivity.kt
+++ jvm-common/src/main/kotlin/org/digma/intellij/plugin/idea/runcfg/RunConfigurationStartupActivity.kt
@@ -5,31 +5,36 @@ import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import org.digma.intellij.plugin.errorreporting.ErrorReporter
 import org.digma.intellij.plugin.posthog.ActivityMonitor
 
 class RunConfigurationStartupActivity : StartupActivity {
     override fun runActivity(project: Project) {
 
 
-        val supported = mutableListOf<String>()
-        val nonSupported = mutableListOf<String>()
+        try {
+            val supported = mutableListOf<String>()
+            val nonSupported = mutableListOf<String>()
 
 
-        val allRunConfigs = RunManager.getInstance(project).allConfigurationsList
-        val tempRunConfigs =
-            RunManager.getInstance(project).tempConfigurationsList.map { runnerAndConfigurationSettings: RunnerAndConfigurationSettings -> runnerAndConfigurationSettings.configuration }
+            val allRunConfigs = RunManager.getInstance(project).allConfigurationsList
+            val tempRunConfigs =
+                RunManager.getInstance(project).tempConfigurationsList.map { runnerAndConfigurationSettings: RunnerAndConfigurationSettings -> runnerAndConfigurationSettings.configuration }
 
-        collect(allRunConfigs, tempRunConfigs, supported, nonSupported)
-
-
-        val details = mapOf(
-            "Supported Run Configs Found" to supported.isNotEmpty(),
-            "supported run configs" to supported.joinToString("; "),
-            "non supported run configs" to nonSupported.joinToString("; ")
-        )
+            collect(allRunConfigs, tempRunConfigs, supported, nonSupported)
 
 
-        ActivityMonitor.getInstance(project).reportSupportedRunConfigDetected(details)
+            val details = mapOf(
+                "Supported Run Configs Found" to supported.isNotEmpty(),
+                "supported run configs" to supported.joinToString("; "),
+                "non supported run configs" to nonSupported.joinToString("; ")
+            )
+
+            ActivityMonitor.getInstance(project).reportSupportedRunConfigDetected(details)
+
+        } catch (e: Throwable) {
+            ErrorReporter.getInstance().reportError("RunConfigurationStartupActivity.runActivity", e)
+        }
     }
 
     private fun collect(

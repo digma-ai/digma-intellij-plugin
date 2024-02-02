0eeb35f224ea10d2efe5c6e60e4ef221d47f268a
oleksandrh <alex.hrebeniuk@gmail.com>
2024-02-02 17:40:30 +0200

    Added EndpointQueryOptimizationInsight

---
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/rest/insights/QueryOptimizationSpan.kt
@@ -0,0 +1,17 @@
+package org.digma.intellij.plugin.model.rest.insights
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import java.beans.ConstructorProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class QueryOptimizationSpan
+@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+@ConstructorProperties("spanInfo", "traceId", "severity", "criticality", "ticketLink")
+constructor(
+    val spanInfo: SpanInfo?,
+    val traceId: String?,
+    val severity: Double,
+    val criticality: Double,
+    var ticketLink: String?,
+)

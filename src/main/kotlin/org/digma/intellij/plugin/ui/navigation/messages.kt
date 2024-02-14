9351e70a6f8e76c3b68c697497dd7e8f5f5fc87b
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-02-14 12:30:00 +0200

    instrumentation result messages

--- src/main/kotlin/org/digma/intellij/plugin/ui/navigation/messages.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/navigation/messages.kt
@@ -3,7 +3,11 @@ package org.digma.intellij.plugin.ui.navigation
 import org.cef.browser.CefBrowser
 import org.digma.intellij.plugin.navigation.View
 import org.digma.intellij.plugin.ui.jcef.serializeAndExecuteWindowPostMessageJavaScript
+import org.digma.intellij.plugin.ui.navigation.model.AddAnnotationResultMessage
+import org.digma.intellij.plugin.ui.navigation.model.AutoFixResultMessage
 import org.digma.intellij.plugin.ui.navigation.model.CodeContextMessage
+import org.digma.intellij.plugin.ui.navigation.model.InstrumentationResult
+import org.digma.intellij.plugin.ui.navigation.model.InstrumentationResultPayload
 import org.digma.intellij.plugin.ui.navigation.model.SetViewMessage
 import org.digma.intellij.plugin.ui.navigation.model.SetViewMessagePayload
 
@@ -19,5 +23,17 @@ fun sendCurrentViewsState(cefBrowser: CefBrowser, views: List<View>) {
 fun sendCodeContext(cefBrowser: CefBrowser, codeContextMessage: CodeContextMessage) {
     serializeAndExecuteWindowPostMessageJavaScript(
         cefBrowser, codeContextMessage
+    )
+}
+
+fun sendAutoFixResultMessage(cefBrowser: CefBrowser, result: InstrumentationResult, error: String? = null) {
+    serializeAndExecuteWindowPostMessageJavaScript(
+        cefBrowser, AutoFixResultMessage(InstrumentationResultPayload(result, error))
+    )
+}
+
+fun sendAddAnnotationResultMessage(cefBrowser: CefBrowser, result: InstrumentationResult, error: String? = null) {
+    serializeAndExecuteWindowPostMessageJavaScript(
+        cefBrowser, AddAnnotationResultMessage(InstrumentationResultPayload(result, error))
     )
 }

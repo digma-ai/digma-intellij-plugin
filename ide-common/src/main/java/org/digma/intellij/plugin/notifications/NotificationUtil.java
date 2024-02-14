7f210249e7068fe8eaf3cce16ca7cd88fe0e7748
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-02-14 12:02:33 +0200

    fading message on autofix failure

--- ide-common/src/main/java/org/digma/intellij/plugin/notifications/NotificationUtil.java
+++ ide-common/src/main/java/org/digma/intellij/plugin/notifications/NotificationUtil.java
@@ -1,18 +1,26 @@
 package org.digma.intellij.plugin.notifications;
 
-import com.intellij.notification.NotificationGroupManager;
-import com.intellij.notification.NotificationType;
+import com.intellij.notification.*;
 import com.intellij.openapi.project.Project;
 
 public class NotificationUtil {
 
     public static final String DIGMA_HIDDEN_NOTIFICATION_GROUP = "Digma Hidden Notification Group";
     public static final String DIGMA_STICKY_BALLOON_NOTIFICATION_GROUP = "Digma Sticky Balloon Notification Group";
+    public static final String DIGMA_FADING_BALLOON_NOTIFICATION_GROUP = "Digma fading Balloon Notification Group";
 
     public static void notifyError(Project project, String content) {
 
         NotificationGroupManager.getInstance()
                 .getNotificationGroup(DIGMA_HIDDEN_NOTIFICATION_GROUP)
+                .createNotification(content, NotificationType.ERROR)
+                .notify(project);
+    }
+
+    public static void notifyFadingError(Project project, String content) {
+
+        NotificationGroupManager.getInstance()
+                .getNotificationGroup(DIGMA_FADING_BALLOON_NOTIFICATION_GROUP)
                 .createNotification(content, NotificationType.ERROR)
                 .notify(project);
     }

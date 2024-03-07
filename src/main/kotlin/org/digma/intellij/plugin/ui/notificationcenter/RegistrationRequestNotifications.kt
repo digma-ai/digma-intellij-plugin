7c0add92f074118ebd1328c6f6f618caf4fdd06c
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-03-07 11:23:48 +0200

    fix typo (#1914)
    * fix typo
    
    * fix typo

--- src/main/kotlin/org/digma/intellij/plugin/ui/notificationcenter/RegistrationRequestNotifications.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/notificationcenter/RegistrationRequestNotifications.kt
@@ -76,7 +76,7 @@ fun showRequestRegistrationNotification() {
 
         val notificationName = "Register"
 
-        ActivityMonitor.getInstance(project).registerNotificationCenterEvent("RequestRegisteration", mapOf())
+        ActivityMonitor.getInstance(project).registerNotificationCenterEvent("Show.$notificationName", mapOf())
 
         val text = asHtml(
             "Please register to continue using Digma.<br>" +

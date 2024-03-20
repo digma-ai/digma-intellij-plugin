24742673d86201689c624d7405cc749dd70d362e
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-03-20 14:11:44 +0200

    add-idea.trace.stub.index.update-to-development-instance (#1972)

--- build.gradle.kts
+++ build.gradle.kts
@@ -244,6 +244,7 @@ tasks {
             "idea.log.debug.categories" to "#org.digma",
             //make large idea.log because the default rotates every 10M and makes it difficult to follow messages with tail
             "idea.log.limit" to "999999999",
+            "idea.trace.stub.index.update" to "true"
 //            "idea.ProcessCanceledException" to "disabled"
         )
 

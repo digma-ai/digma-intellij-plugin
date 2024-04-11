02ab7d926c5e78d78aed3ed18f035cf7f9c6319d
olehp <opoliarush@digma.ai>
2024-04-11 16:43:23 +0300

    Revert "Update UI"
    This reverts commit 907879565d85315526716d5bb4b25cced93f276f.

--- ide-common/src/main/kotlin/org/digma/intellij/plugin/posthog/enums.kt
+++
@@ -1,3 +0,0 @@
-package org.digma.intellij.plugin.posthog
-
-enum class UserActionOrigin { Assets, Insights, Dashboard, Tests, RecentActivity, JaegerUI, NoConnectionPanel, LoadStatusPanel, ErrorDetails }

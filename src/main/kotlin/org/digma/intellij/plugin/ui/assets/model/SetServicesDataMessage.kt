a5391920143893f3122e6342d1d4dd1bd4359692
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-04-15 20:46:59 +0300

    remove unnecessary jcef variables

--- src/main/kotlin/org/digma/intellij/plugin/ui/assets/model/SetServicesDataMessage.kt
+++
@@ -1,9 +0,0 @@
-package org.digma.intellij.plugin.ui.assets.model
-
-import com.fasterxml.jackson.databind.JsonNode
-import org.digma.intellij.plugin.ui.jcef.JCEFGlobalConstants
-
-data class SetServicesDataMessage(val payload: JsonNode) {
-    val type = JCEFGlobalConstants.REQUEST_MESSAGE_TYPE
-    val action = "ASSETS/SET_SERVICES"
-}

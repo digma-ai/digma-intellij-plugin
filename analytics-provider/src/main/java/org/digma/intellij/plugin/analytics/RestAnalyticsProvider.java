53866deef6779551f5a42287cc4ebae2a208aef2
Kyrylo Shmidt <kshmidt@digma.ai>
2024-04-15 18:47:31 +0200

    Fix imports

--- analytics-provider/src/main/java/org/digma/intellij/plugin/analytics/RestAnalyticsProvider.java
+++ analytics-provider/src/main/java/org/digma/intellij/plugin/analytics/RestAnalyticsProvider.java
@@ -16,7 +16,6 @@ import org.digma.intellij.plugin.model.rest.environment.Env;
 import org.digma.intellij.plugin.model.rest.errordetails.CodeObjectErrorDetails;
 import org.digma.intellij.plugin.model.rest.errors.CodeObjectError;
 import org.digma.intellij.plugin.model.rest.event.*;
-import org.digma.intellij.plugin.model.rest.highlights.HighlightsPerformanceResponse;
 import org.digma.intellij.plugin.model.rest.insights.*;
 import org.digma.intellij.plugin.model.rest.livedata.*;
 import org.digma.intellij.plugin.model.rest.login.*;

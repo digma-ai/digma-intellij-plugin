747ac956d89c74aabab10a5cf10a8e8382b7466e
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-07-08 15:48:28 +0300

    fix-error-in-242

--- jvm-common/src/main/kotlin/org/digma/intellij/plugin/idea/execution/RunConfigurationHandlersHolder.kt
+++ jvm-common/src/main/kotlin/org/digma/intellij/plugin/idea/execution/RunConfigurationHandlersHolder.kt
@@ -1,26 +1,44 @@
 package org.digma.intellij.plugin.idea.execution
 
+import com.intellij.openapi.components.service
 import org.digma.intellij.plugin.errorreporting.ErrorReporter
 import org.digma.intellij.plugin.execution.RunConfigurationInstrumentationHandler
 import java.util.ServiceLoader
 
-object RunConfigurationHandlersHolder {
+@Suppress("LightServiceMigrationCode")
+class RunConfigurationHandlersHolder {
+
+    private var runConfigurationHandlers: List<RunConfigurationInstrumentationHandler>? = null
+
+    companion object {
+        fun getInstance(): RunConfigurationHandlersHolder {
+            return service<RunConfigurationHandlersHolder>()
+        }
+    }
+
 
     //RunConfigurationInstrumentationHandler is a delegating facades to services that provide the real work.
     //the main reason is to hide gradle and maven classes from the plugin in case gradle or maven are disabled.
     //RunConfigurationInstrumentationHandler that don't need a service can do the work themselves.
-    val runConfigurationHandlers: List<RunConfigurationInstrumentationHandler> = try {
-        //ServiceLoader doesn't work in intellij as it is in any other java application.
-        //see https://youtrack.jetbrains.com/issue/IDEA-241229
-        //so changing the context class loader for loading is the workaround
-        val currentClassLoader = Thread.currentThread().contextClassLoader
-        Thread.currentThread().setContextClassLoader(this::class.java.classLoader)
-        val handlers =
-            ServiceLoader.load(RunConfigurationInstrumentationHandler::class.java).stream().map { it.get() }.toList()
-        Thread.currentThread().setContextClassLoader(currentClassLoader)
-        handlers
-    } catch (e: Throwable) {
-        ErrorReporter.getInstance().reportInternalFatalError("RunConfigurationHandlersHolder.init", e)
-        listOf()
+    fun getRunConfigurationHandlers(): List<RunConfigurationInstrumentationHandler> {
+        if (runConfigurationHandlers == null) {
+
+            //ServiceLoader doesn't work in intellij as it is in any other java application.
+            //see https://youtrack.jetbrains.com/issue/IDEA-241229
+            //so changing the context class loader for loading is the workaround
+            val currentClassLoader = Thread.currentThread().contextClassLoader
+
+            runConfigurationHandlers = try {
+                Thread.currentThread().setContextClassLoader(this::class.java.classLoader)
+                ServiceLoader.load(RunConfigurationInstrumentationHandler::class.java).stream().map { it.get() }.toList()
+            } catch (e: Throwable) {
+                ErrorReporter.getInstance().reportInternalFatalError("RunConfigurationHandlersHolder.init", e)
+                emptyList()
+            } finally {
+                Thread.currentThread().setContextClassLoader(currentClassLoader)
+            }
+        }
+        return runConfigurationHandlers ?: emptyList()
     }
+
 }

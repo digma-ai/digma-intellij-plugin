0057788542d72c2fcb9b18641ed5fba555489b4e
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-07-18 16:14:45 +0300

    no auth

--- ide-common/src/main/kotlin/org/digma/intellij/plugin/auth/HeaderTokenAuthenticationProvider.kt
+++ ide-common/src/main/kotlin/org/digma/intellij/plugin/auth/HeaderTokenAuthenticationProvider.kt
@@ -5,11 +5,12 @@ import org.digma.intellij.plugin.analytics.AuthenticationProvider
 class HeaderTokenAuthenticationProvider(private val tokenProvider: TokenProvider) : AuthenticationProvider {
 
     override fun getHeaderName(): String {
+//        return "Authorization"
         return "Authorization"
     }
 
     override fun getHeaderValue(): String? {
-        return tokenProvider.provideToken()
+        return null
     }
 
 }

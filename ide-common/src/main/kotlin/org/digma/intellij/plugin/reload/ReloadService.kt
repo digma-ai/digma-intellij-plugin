6f8cdf7af22eeb884796b4fbf0dffb380fdb1ef7
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-08-28 12:08:18 +0300

    reload UI

--- ide-common/src/main/kotlin/org/digma/intellij/plugin/reload/ReloadService.kt
+++ ide-common/src/main/kotlin/org/digma/intellij/plugin/reload/ReloadService.kt
@@ -2,9 +2,12 @@ package org.digma.intellij.plugin.reload
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.util.Disposer
 import org.digma.intellij.plugin.common.EDT
+import org.digma.intellij.plugin.common.isProjectValid
 import org.digma.intellij.plugin.errorreporting.ErrorReporter
+import org.digma.intellij.plugin.ui.MainToolWindowCardsController
 import org.digma.intellij.plugin.ui.panels.ReloadablePanel
 
 @Service(Service.Level.APP)
@@ -26,6 +29,16 @@ class ReloadService {
 
 
     fun reload() {
+        ProjectManager.getInstance().openProjects.forEach {
+            try {
+                if (isProjectValid(it)) {
+                    MainToolWindowCardsController.getInstance(it).wizardFinished()
+                }
+            } catch (e: Throwable) {
+                ErrorReporter.getInstance().reportError("ReloadService.reload", e)
+            }
+        }
+
         reloadables.forEach {
             EDT.ensureEDT {
                 try {

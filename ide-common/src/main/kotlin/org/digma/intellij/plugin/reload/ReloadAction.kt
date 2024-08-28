7b1f947bf93bc33dd8232aaffae5172a1fe205bb
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-08-28 03:10:54 +0300

    reload UI

--- ide-common/src/main/kotlin/org/digma/intellij/plugin/reload/ReloadAction.kt
+++ ide-common/src/main/kotlin/org/digma/intellij/plugin/reload/ReloadAction.kt
@@ -5,6 +5,8 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.components.service
 import org.digma.intellij.plugin.errorreporting.ErrorReporter
 
+//devkit doesn't know that the action is registered because it looks for plugin.xml in the current project
+@Suppress("ComponentNotRegistered")
 class ReloadAction : AnAction() {
     override fun actionPerformed(p0: AnActionEvent) {
         try {

b917405c96b7dd25f3a423fa17e0f4ebf02818e7
opoliarush <opoliarush@digma.ai>
2024-09-03 16:45:01 +0300

    Fix tab switch

--- src/main/kotlin/org/digma/intellij/plugin/ui/insights/model/SetInsightDataListMessage.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/insights/model/SetInsightDataListMessage.kt
@@ -1,10 +1,22 @@
 package org.digma.intellij.plugin.ui.insights.model
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonRawValue
 import org.digma.intellij.plugin.ui.jcef.JCEFGlobalConstants
 import org.digma.intellij.plugin.ui.jcef.model.ErrorPayload
+import java.beans.ConstructorProperties
 
-data class SetInsightDataListMessage( @JsonRawValue val payload: String,val error: ErrorPayload? = null) {
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class SetInsightsData
+@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+@ConstructorProperties("data", "viewMode")
+constructor(
+    @JsonRawValue val data: String,
+    val insightsViewMode: String
+)
+
+data class SetInsightDataListMessage(val payload: SetInsightsData, val error: ErrorPayload? = null) {
     val type = JCEFGlobalConstants.REQUEST_MESSAGE_TYPE
     val action = "INSIGHTS/SET_DATA_LIST"
 }

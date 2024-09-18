df35d542fa7da4b2470469461e2292bcfe341ed6
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-09-18 17:52:24 +0300

    kotlin 2

--- building-how-to/build-all-profiles-short.sh
+++ building-how-to/build-all-profiles-short.sh
@@ -5,22 +5,22 @@ set -e
 
 echo "############ building with 231 ################"
 ./gradlew clean buildPlugin -PbuildProfile=p231
-cp build/distributions/digma-intellij-plugin*.zip .
+cp build/distributions/digma-intellij-plugin*.zip ./.tmp
 for i in {1..5}; do echo; done
 
 echo "############ building with 232 ################"
 ./gradlew clean buildPlugin -PbuildProfile=p232
-cp build/distributions/digma-intellij-plugin*.zip .
+cp build/distributions/digma-intellij-plugin*.zip ./.tmp
 for i in {1..5}; do echo; done
 
 echo "############ building with 233 ################"
 ./gradlew clean buildPlugin -PbuildProfile=p233
-cp build/distributions/digma-intellij-plugin*.zip .
+cp build/distributions/digma-intellij-plugin*.zip ./.tmp
 for i in {1..5}; do echo; done
 
 echo "############ building with 241 ################"
 ./gradlew clean buildPlugin -PbuildProfile=p241
-cp build/distributions/digma-intellij-plugin*.zip .
+cp build/distributions/digma-intellij-plugin*.zip ./.tmp
 for i in {1..5}; do echo; done
 
 echo "############ building with 242 ################"
@@ -29,4 +29,4 @@ cp build/distributions/digma-intellij-plugin*.zip .
 
 echo "############ building with 243 ################"
 ./gradlew clean buildPlugin -PbuildProfile=p243
-cp build/distributions/digma-intellij-plugin*.zip .
+cp build/distributions/digma-intellij-plugin*.zip ./.tmp

ec266e7e81d40970cec41f4638556f364aa78eb5
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-11-04 20:15:37 +0200

    fix-docker-command-on-windows

--- ide-common/src/main/kotlin/org/digma/intellij/plugin/docker/CmdHelper.kt
+++ ide-common/src/main/kotlin/org/digma/intellij/plugin/docker/CmdHelper.kt
@@ -12,36 +12,12 @@ const val DOCKER_COMPOSE_COMMAND = "docker-compose"
 
 private val logger = Logger.getInstance("org.digma.intellij.plugin.docker.CmdHelper")
 
-fun getCommand(program: String): String? {
 
-    Log.log(logger::debug, "getCommand invoked for {}", program)
-
-    val cmd = GeneralCommandLine(WHICH_COMMAND, program)
-        .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
-
-    try {
-        val result = ExecUtil.execAndReadLine(cmd)
-        Log.log(logger::debug, "getCommand.getExecPath: {} result with: {}", cmd.commandLineString, result)
-        return result
-    } catch (ex: Exception) {
-        Log.warnWithException(logger, ex, "getCommand Failed to run '{}'", cmd.commandLineString)
-    }
-    return null
-}
 
 
 fun getDockerCommand(): String? {
-
     Log.log(logger::debug, "getDockerCommand invoked")
-
-    var dockerCmd = getCommand(DOCKER_COMMAND)
-    if (dockerCmd != null) {
-        if (SystemInfo.isWindows && !dockerCmd.endsWith("exe", true)) {
-            dockerCmd = dockerCmd.plus(".exe")
-        }
-        return dockerCmd
-    }
-    return null
+    return getCommand(DOCKER_COMMAND)
 }
 
 
@@ -49,20 +25,40 @@ fun getDockerComposeCommand(): List<String>? {
 
     Log.log(logger::debug, "getDockerComposeCommand invoked")
 
-    var dockerComposeCmd = getCommand(DOCKER_COMPOSE_COMMAND)
+    val dockerComposeCmd = getCommand(DOCKER_COMPOSE_COMMAND)
     if (dockerComposeCmd != null) {
-        if (SystemInfo.isWindows && !dockerComposeCmd.endsWith("exe", true)) {
-            dockerComposeCmd = dockerComposeCmd.plus(".exe")
-        }
         return listOf(dockerComposeCmd)
     }
 
-    var dockerCmd = getCommand(DOCKER_COMMAND)
+    val dockerCmd = getCommand(DOCKER_COMMAND)
     if (dockerCmd != null) {
-        if (SystemInfo.isWindows && !dockerCmd.endsWith("exe", true)) {
-            dockerCmd = dockerCmd.plus(".exe")
-        }
         return listOf(dockerCmd, "compose")
+    }
+    return null
+}
+
+
+private fun getCommand(program: String): String? {
+
+    Log.log(logger::debug, "getCommand invoked for {}", program)
+
+    val cmd = GeneralCommandLine(WHICH_COMMAND, program)
+        .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
+
+    try {
+        val result = if (SystemInfo.isWindows) {
+            //on windows the result of 'where docker' may return two lines, ../docker and ..docker.exe
+            val output = ExecUtil.execAndGetOutput(cmd).stdoutLines
+            //prefer line that ends with exe or bat, or just the first one if none found
+            output.first { it.endsWith(".exe", true) || it.endsWith(".bat", true) } ?: output.first()
+        }else{
+            ExecUtil.execAndReadLine(cmd)
+        }
+
+        Log.log(logger::debug, "getCommand {} result with {}", cmd.commandLineString, result)
+        return result
+    } catch (ex: Exception) {
+        Log.warnWithException(logger, ex, "getCommand Failed to run '{}'", cmd.commandLineString)
     }
     return null
 }

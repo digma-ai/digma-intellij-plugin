b13cf136cfde26b3ad0c6ac11dd700e6f3de88a2
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-11-05 11:37:00 +0200

    workaround for missing intellij modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,6 +90,7 @@ dependencies {
 
         //todo: this is a workaround for plugin 2.1.0, this module should be bundled.
         // check in next version if it is still necessary.
+        // https://jetbrains-platform.slack.com/archives/C05C80200LS/p1730794028550679
         if (project.currentProfile().profile.greaterThan(BuildProfiles.Profile.p241)) {
             bundledModule("intellij.platform.collaborationTools")
         }

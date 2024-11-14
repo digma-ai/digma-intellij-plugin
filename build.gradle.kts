c65637f6891343afd54ebda7bbc1058c6d1e85a8
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-11-14 11:30:23 +0200

    update build profile 243

--- build.gradle.kts
+++ build.gradle.kts
@@ -253,7 +253,7 @@ tasks {
         //to upgrade gradle change the version here and run:
         //./gradlew wrapper --gradle-version 8.8
         //check that gradle/wrapper/gradle-wrapper.properties was changed
-        gradleVersion = "8.10"
+        gradleVersion = "8.11"
         distributionType = Wrapper.DistributionType.ALL
         distributionBase = Wrapper.PathBase.GRADLE_USER_HOME
         distributionPath = "wrapper/dists"

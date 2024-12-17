db540b57cc784c5ff7030780d1a0c116e69d22ab
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-12-17 17:56:31 +0200

    fix base directory

--- ide-common/src/main/kotlin/org/digma/intellij/plugin/paths/DigmaPathManager.kt
+++ ide-common/src/main/kotlin/org/digma/intellij/plugin/paths/DigmaPathManager.kt
@@ -2,8 +2,11 @@ package org.digma.intellij.plugin.paths
 
 import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.application.PathManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.util.SystemInfo
+import org.apache.commons.codec.digest.DigestUtils
 import org.digma.intellij.plugin.errorreporting.ErrorReporter
+import org.digma.intellij.plugin.log.Log
 import java.io.File
 
 const val DIGMA_DIR = "digma-intellij-plugin"
@@ -13,19 +16,40 @@ class DigmaPathManager {
 
     companion object {
 
+        private val logger = Logger.getInstance(this::class.java)
+
+        /**
+         * returns the base directory to save files belonging to digma installation
+         */
         fun getLocalFilesDirectoryPath(): String {
+
+            Log.log(logger::trace, "getLocalFilesDirectoryPath called")
+
             val ideFullName = ApplicationNamesInfo.getInstance().fullProductNameWithEdition
-            val ideHomeDir = PathManager.getHomePath().substringAfterLast("/")
-            val ideName = "$ideFullName-$ideHomeDir".replace(" ", "-").replace(".","-")
+            val ideHomeDir = PathManager.getHomePath()
+            val ideHomeDirHash = DigestUtils.sha1Hex(ideHomeDir)
+            val ideName = "$ideFullName-$ideHomeDirHash".replace(" ", "-").replace(".", "-")
+
+            Log.log(
+                logger::trace,
+                "building ide name from: ideFullName={},ideHomeDir={},ideHomeDirHash={}. resulting ide name is: {}",
+                ideFullName,
+                ideHomeDir,
+                ideHomeDirHash,
+                ideName
+            )
+
             return try {
                 val baseDir = getBaseDirectory()
-                val ideDir = File(baseDir,ideName)
+                val ideDir = File(baseDir, ideName)
                 ideDir.mkdirs()
+                Log.log(logger::trace, "getLocalFilesDirectoryPath created ide dir {}", ideDir.absolutePath)
                 ideDir.absolutePath
-            }catch (e:Throwable){
+            } catch (e: Throwable) {
                 ErrorReporter.getInstance().reportError("DigmaPathManager.getLocalFilesDirectoryPath", e)
                 val fallback = File(System.getProperty("java.io.tmpdir"), "$DIGMA_DIR/$ideName")
                 fallback.mkdirs()
+                Log.warnWithException(logger, e, "getLocalFilesDirectoryPath failed, using fallback location {}", fallback.absolutePath)
                 fallback.absolutePath
             }
         }
@@ -39,22 +63,27 @@ class DigmaPathManager {
 
         private fun getBaseDirectory(): File {
 
+            Log.log(logger::trace, "getBaseDirectory called")
+
             return try {
                 if (SystemInfo.isMac) {
                     val userHome = getUserHome()
                     val digmaDir = File(userHome, "Library/Application Support/$DIGMA_DIR")
                     digmaDir.mkdirs()
+                    Log.log(logger::trace, "os is mac, using base directory {}",digmaDir)
                     digmaDir
                 } else if (SystemInfo.isWindows) {
                     val userHome = System.getenv("LOCALAPPDATA") ?: getUserHome()
                     val digmaDir = File(userHome, DIGMA_DIR)
                     digmaDir.mkdirs()
+                    Log.log(logger::trace, "os is windows, using base directory {}",digmaDir)
                     digmaDir
                 } else {
                     //for linux or any other os
                     val userHome = getUserHome()
                     val digmaDir = File(userHome, ".$DIGMA_DIR")
                     digmaDir.mkdirs()
+                    Log.log(logger::trace, "os is ${SystemInfo.OS_NAME}, using base directory {}",digmaDir)
                     digmaDir
                 }
 
@@ -63,6 +92,7 @@ class DigmaPathManager {
                 val userHome = getUserHome()
                 val digmaDir = File(userHome, ".$DIGMA_DIR")
                 digmaDir.mkdirs()
+                Log.warnWithException(logger, e, "getBaseDirectory failed, using fallback location {}", digmaDir)
                 digmaDir
             }
         }

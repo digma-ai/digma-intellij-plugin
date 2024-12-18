79a751ab88547b185ebb793f54ab7bcdac5ec17a
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-12-18 18:09:35 +0200

    env.js - ui 5.0.0

--- common-build-logic/src/main/kotlin/common/BuildProfile.kt
+++ common-build-logic/src/main/kotlin/common/BuildProfile.kt
@@ -219,7 +219,7 @@ object BuildProfiles {
         Profile.p243 to BuildProfile(
             profile = Profile.p243,
             platformVersion = "2024.3.1",
-            riderVersion = "2024.3",
+            riderVersion = "2024.3.2",
             pycharmVersion = "2024.3",
             riderTargetFramework = "net8.0",
             riderResharperVersionConstant = "PROFILE_2023_2;PROFILE_2024_3",

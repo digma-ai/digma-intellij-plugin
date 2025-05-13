56ceb392473e3011a3c646dd6cc36f1379e88bb5
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2025-05-13 16:50:37 +0300

    fix lifetime

--- rider/src/main/kotlin/org/digma/intellij/plugin/rider/protocol/ShowToolWindowHost.kt
+++ rider/src/main/kotlin/org/digma/intellij/plugin/rider/protocol/ShowToolWindowHost.kt
@@ -1,18 +1,18 @@
 package org.digma.intellij.plugin.rider.protocol
 
 import com.intellij.openapi.project.Project
-import com.jetbrains.rd.platform.util.lifetime
 import com.jetbrains.rdclient.util.idea.LifetimedProjectComponent
 import com.jetbrains.rider.projectView.solution
 import org.digma.intellij.plugin.ui.ToolWindowShower
 
+@Suppress("LightServiceMigrationCode")
 class ShowToolWindowHost(project: Project) : LifetimedProjectComponent(project) {
 
     private val model: ShowToolWindowModel = project.solution.showToolWindowModel
 
 
     init {
-        model.showToolWindow.advise(project.lifetime){
+        model.showToolWindow.advise(componentLifetime){
             ToolWindowShower.getInstance(project).showToolWindow()
         }
     }

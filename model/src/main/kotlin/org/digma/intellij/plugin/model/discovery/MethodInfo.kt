832c5ecf02b433da57aafe6ef305797befc1312f
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2025-05-18 20:25:00 +0300

    fix DocumentInfo equals

--- model/src/main/kotlin/org/digma/intellij/plugin/model/discovery/MethodInfo.kt
+++ model/src/main/kotlin/org/digma/intellij/plugin/model/discovery/MethodInfo.kt
@@ -7,38 +7,33 @@ data class MethodInfo(
     override val id: String, // CodeObjectId (without type (prefix of 'method:'))
     val name: String,
     val containingClass: String,
-    val containingNamespace: String, //namespace for c# is namespace, for java its the package
+    val containingNamespace: String, //namespace for c# is namespace, for java its package
     val containingFileUri: String,
     val offsetAtFileUri: Int,
+    //mutable spans and endpoints are a compromise. we want them to be included in hash code and equals,
+    // so they need to be constructor properties. ideally it would be better if they were immutable, but we also
+    // want to add to them. every other solution involves declaring other fields, copies, etc.
+    val spans: MutableList<SpanInfo> = mutableListOf(),
+    val endpoints: MutableList<EndpointInfo> = mutableListOf(),
 ) : CodeObjectInfo {
-
-    private val innerSpans: MutableList<SpanInfo> = mutableListOf()
-
-    val spans: List<SpanInfo>
-        get() = innerSpans
-
-    private val innerEndpoints: MutableList<EndpointInfo> = mutableListOf()
-
-    val endpoints: List<EndpointInfo>
-        get() = innerEndpoints
 
     /*
         Note about id:
-        a method has a code object id, its language dependent. and usually this id is used to get insights from
+        a method has a code object id, its language dependent. And usually this id is used to get insights from
         the backend and to match a method to insights when the cursor moves between methods.
-        in java and C# only this one id is necessary.
-        But in python we need to use few patterns for the id. so a method has a main id, but when sending insights request
-        we need to send multiple patters that are based on the main id. and when matching method id when the cursor moves
-        between methods we need to match to multiple patterns.
+        In java and C# only this one id is necessary.
+        But in python we need to use few patterns for the id. So a method has a main id, but when sending insights request,
+        we need to send multiple patters that are based on the main id. And when matching method id when the cursor moves
+        between methods, we need to match to multiple patterns.
         So we have an AdditionalIdsProvider , the default for java and C# does nothing and returns an empty list.
         But PythonAdditionalIdsProvider returns multiple ids.
-        and MethodInfo has two corresponding methods: allIdsWithType and allIdsWithoutType.
-        care should be taken when using one or the other.
+        And MethodInfo has two corresponding methods: allIdsWithType and allIdsWithoutType.
+        Care should be taken when using one or the other.
 
      */
 
 
-    //we need AdditionalIdsProvider only for python. this default implementation returns empty list
+    //we need AdditionalIdsProvider only for python. this default implementation returns an empty list
     var additionalIdsProvider: AdditionalIdsProvider = object : AdditionalIdsProvider {
         override fun provideAdditionalIdsWithType(methodInfo: MethodInfo): List<String> {
             return listOf()
@@ -102,15 +97,15 @@ data class MethodInfo(
     }
 
     fun addSpan(theSpan: SpanInfo) {
-        innerSpans.add(theSpan)
+        spans.add(theSpan)
     }
 
     fun addSpans(theSpans: Collection<SpanInfo>) {
-        innerSpans.addAll(theSpans)
+        spans.addAll(theSpans)
     }
 
     fun addEndpoint(endpoint: EndpointInfo) {
-        innerEndpoints.add(endpoint)
+        endpoints.add(endpoint)
     }
 
     fun buildDisplayName(): String {

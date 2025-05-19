8f3277d233a7a0f892d47880c1f58f184b4ab142
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2025-05-19 23:16:06 +0300

    fix-semantic-version-plugin

--- common-build-logic/src/main/kotlin/semantic-version.gradle.kts
+++ common-build-logic/src/main/kotlin/semantic-version.gradle.kts
@@ -1,20 +1,24 @@
 tasks.register("printSemanticVersion") {
+    notCompatibleWithConfigurationCache("printSemanticVersion is not yet compatible with configuration cache")
     doLast {
         println("${project.name} ${common.semanticversion.getSemanticVersion(project)}")
     }
 }
 
 tasks.register("incrementSemanticVersionPatch") {
+    notCompatibleWithConfigurationCache("incrementSemanticVersionPatch is not yet compatible with configuration cache")
     doLast {
         common.semanticversion.incrementSemanticVersionPatch(project)
     }
 }
 tasks.register("incrementSemanticVersionMinor") {
+    notCompatibleWithConfigurationCache("incrementSemanticVersionMinor is not yet compatible with configuration cache")
     doLast {
         common.semanticversion.incrementSemanticVersionMinor(project)
     }
 }
 tasks.register("incrementSemanticVersionMajor") {
+    notCompatibleWithConfigurationCache("incrementSemanticVersionMajor is not yet compatible with configuration cache")
     doLast {
         common.semanticversion.incrementSemanticVersionMajor(project)
     }

afe0da64424b449a8f0d49ae4b792cfda7f4576d
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2025-08-13 00:02:51 +0300

    update build profiles

--- common-build-logic/src/main/kotlin/common/BuildProfile.kt
+++ common-build-logic/src/main/kotlin/common/BuildProfile.kt
@@ -201,6 +201,7 @@ object BuildProfiles {
         ),
 
         Profile.p252 to BuildProfile(
+            isEAP = true,
             profile = Profile.p252,
             platformVersion = "2025.2",
             riderVersion = "2025.2-RC2-SNAPSHOT",
